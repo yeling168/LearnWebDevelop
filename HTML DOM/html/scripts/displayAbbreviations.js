9b1c99391b00111fb1a39a6f1f9cbd5c61af811d
study0918 <beyondthestars@yeah.net>
2019-01-09 23:01:37 +0800

    'abbr'

--- HTML DOM/html/scripts/displayAbbreviations.js
+++ HTML DOM/html/scripts/displayAbbreviations.js
@@ -13,7 +13,6 @@ function displayAbbreviations() {
         var key = current_abbr.lastChild.nodeValue;
         defs[key] = definition;
     }
-    console.log(defs);
     //创建定义列表
     var dlist = document.createElement("dl");
     for (key in defs) {
@@ -39,4 +38,83 @@ function displayAbbreviations() {
     document.body.appendChild(dlist);
 }
 
+function displayCitations() {
+    if (!document.getElementsByTagName || !document.createElement || !document.createTextNode) {
+        return false;
+    }
+    //取得所有引用
+    var quotes = document.getElementsByTagName("blockquote");
+    //遍历引用
+    for (var i = 0; i < quotes.length; i++) {
+        //如果没有cite属性，继续循环
+        if (!quotes[i].getAttribute("cite")) {
+            continue;
+        }
+        //保存cite属性
+        var url = quotes[i].getAttribute("cite");
+        //取得引用中的所有元素节点
+        var quoteChildren = quotes[i].getElementsByTagName("*");
+        //如果没有元素节点，继续循环
+        if (quoteChildren.length < 1) continue;
+        //取得引用中的最后一个元素节点
+        var elem = quoteChildren[quoteChildren.length - 1];
+        //创建标记
+        var link = document.createElement("a");
+        var link_text = document.createTextNode("source");
+        link.appendChild(link_text);
+        link.setAttribute("href", url);
+        var superscript = document.createElement("sup");
+        superscript.appendChild(link);
+        //把标记添加到引用中的最后一个元素节点
+        elem.appendChild(superscript);
+    }
+}
+
+function displayAccesskeys() {
+    if (!document.getElementsByTagName || !document.createElement || !document.createTextNode) {
+        return false;
+    }
+    //取得文档中的所有链接
+    var links = document.getElementsByTagName("a");
+    //创建一个数组，保存访问键
+    var akeys = new Array();
+    //遍历链接
+    for (var i = 0; i < links.length; i++) {
+        var current_link = links[i];
+        //如果没有accesskey属性，继续循环
+        if (!current_link.getAttribute("accesskey")) continue;
+        //取得accesskey的值
+        var key = current_link.getAttribute("accesskey");
+        //取得链接文本
+        var text = current_link.lastChild.nodeValue;
+        //添加到数组
+        akeys[key] = text;
+    }
+    console.log(akeys);
+    //创建列表
+    var list = document.createElement("ul");
+    //遍历访问键
+    for (key in akeys) {
+        var text = akeys[key];
+        //创建放到列表项中的字符串
+        var str = key + ":" + text;
+        //创建列表项
+        var item = document.createElement("li");
+        var item_text = document.createTextNode(str);
+        item.appendChild(item_text);
+        //把列表项添加到列表中
+        list.appendChild(item);
+    }
+    //创建标题
+    var header = document.createElement("h3");
+    var header_text = document.createTextNode("Accesskeys");
+    header.appendChild(header_text);
+    //把标题添加到页面主体
+    document.body.appendChild(header);
+    //把列表添加到页面主体
+    document.body.appendChild(list);
+}
+
+addLoadEvent(displayCitations);
 addLoadEvent(displayAbbreviations);
+addLoadEvent(displayAccesskeys);
