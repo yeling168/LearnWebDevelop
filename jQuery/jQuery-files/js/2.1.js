0885e252e12e183ab271e95c3481e4ba5b78f449
study0918 <beyondthestars@yeah.net>
2019-01-17 00:09:54 +0800

    'jQuery'

---
+++ jQuery/jQuery-files/js/2.1.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('#selected-plays > li').addClass('horizontal');
+    $('#selected-plays li:not(.horizontal)').addClass('sub-level');
+});
+
+$(document).ready(function () {
+    $('a[href^="mailto:"]').addClass('mailto');
+    $('a[href$=".pdf"]').addClass('pdflink');
+});
+
+$(document).ready(function () {
+    $('a[href^="mailto:"]').addClass('mailto');
+    $('a[href$=".pdf"]').addClass('pdflink');
+    $('a[href^="http"][href*="henry"]')
+        .addClass('henrylink');
+});
