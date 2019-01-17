9ccc39d6f24334b9d0a256c6c31ac5c43ada1624
study0918 <beyondthestars@yeah.net>
2019-01-17 22:20:13 +0800

    'event'

--- jQuery/jQuery-files/js/2.1.js
+++ jQuery/jQuery-files/js/2.1.js
@@ -14,3 +14,13 @@ $(document).ready(function () {
     $('a[href^="http"][href*="henry"]')
         .addClass('henrylink');
 });
+
+$(document).ready(function () {
+    //$("td:contains(Henry)").nextAll().addClass("highlight");
+    //$("td:contains(Henry)").siblings().addClass("highlight");
+    //$("td:contains(Henry)").nextAll().addClass("highlight");
+    //$("td:contains(Henry)").nextAll().addBack().addClass("highlight");
+    //$('td:contains(Henry)').parent().children().addClass('highlight');
+    $('td:contains(Henry)').parent().find('td:eq(1)').addClass('highlight').end().find('td:eq(2)')
+    .addClass('highlight');
+});
