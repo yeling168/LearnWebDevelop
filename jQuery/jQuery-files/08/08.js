9522490c71092d968d1192d3c4bda47374e4dd47
study0918 <beyondthestars@yeah.net>
2019-02-20 23:04:07 +0800

    'push'

--- jQuery/jQuery-files/08/08.js
+++ jQuery/jQuery-files/08/08.js
@@ -93,6 +93,90 @@
             }
         });
     };
+
+    // $.fn.shadow = function () {
+    //     return this.each(function () {
+    //         var $originalElement = $(this);
+    //         for (var i = 0; i < 5; i++) {
+    //             $originalElement.clone().css({
+    //                 position: 'absolute',
+    //                 left: $originalElement.offset().left + i,
+    //                 top: $originalElement.offset().top + i,
+    //                 margin: 0,
+    //                 zIndex: -1,
+    //                 opacity: 0.1
+    //             }).appendTo('body');
+    //         }
+    //     })
+    // }
+
+    // $.fn.shadow = function () {
+    //     return this.each(function (options) {
+    //         var $originalElement = $(this);
+    //         for (var i = 0; i < options.copies; i++) {
+    //             $originalElement.clone().css({
+    //                 position: 'absolute',
+    //                 left: $originalElement.offset().left + i,
+    //                 top: $originalElement.offset().top + i,
+    //                 margin: 0,
+    //                 zIndex: -1,
+    //                 opacity: options.opacity
+    //             }).appendTo('body');
+    //         }
+    //     })
+    // }
+
+    // $.fn.shadow = function () {
+    //     return this.each(function (opts) {
+    //         var defaults = {
+    //             copies: 5,
+    //             opacity: 0.1
+    //         };
+    //         var options = $.extend(defaults, opts);
+    //         var $originalElement = $(this);
+    //         for (var i = 0; i < options.copies; i++) {
+    //             $originalElement.clone().css({
+    //                 position: 'absolute',
+    //                 left: $originalElement.offset().left + i,
+    //                 top: $originalElement.offset().top + i,
+    //                 margin: 0,
+    //                 zIndex: -1,
+    //                 opacity: options.opacity
+    //             }).appendTo('body');
+    //         }
+    //     })
+    // }
+
+    // $.fn.shadow = function (opts) {
+    //     var defaults = {
+    //         copies: 5,
+    //         opacity: 0.1,
+    //         copyOffset: function (index) {
+    //             return {
+    //                 x: index,
+    //                 y: index
+    //             };
+    //         }
+    //     };
+    //     var options = $.extend(defaults, opts);
+    //     return this.each(function () {
+    //         var $originalElement = $(this);
+    //         for (var i = 0; i < options.copies; i++) {
+    //             var offset = options.copyOffset(i);
+    //             $originalElement
+    //                 .clone()
+    //                 .css({
+    //                     position: 'absolute',
+    //                     left: $originalElement.offset().left + offset.x,
+    //                     top: $originalElement.offset().top + offset.y,
+    //                     margin: 0,
+    //                     zIndex: -1,
+    //                     opacity: options.opacity
+    //                 })
+    //                 .appendTo('body');
+    //         }
+    //     });
+    // };
 })(jQuery);
 
 $(document).ready(function () {
@@ -100,6 +184,47 @@ $(document).ready(function () {
         $('tr').swapClass('one', 'two');
     })
 })
+
+// $(document).ready(function(){
+//     $('h1').shadow();
+// })
+
+// $(document).ready(function () {
+//     $('h1').shadow({
+//         copies: 3,
+//         opacity: 0.25
+//     });
+// })
+
+// $(document).ready(function () {
+//     $('h1').shadow({
+//         copies: 3,
+//     });
+// })
+
+// $(document).ready(function () {
+//     $('h1').shadow({
+//         copyOffset: function (index) {
+//             return {
+//                 x: -index,
+//                 y: -2 * index
+//             };
+//         }
+//     });
+// });
+
+// $(document).ready(function () {
+//     $.fn.shadow.defaults.copies = 10;
+//     $('h1').shadow({
+//         copyOffset: function (index) {
+//             return {
+//                 x: -index,
+//                 y: index
+//             };
+//         }
+//     });
+// });
+
 
 $(document).ready(function () {
     var $inventory = $('#inventory tbody');
