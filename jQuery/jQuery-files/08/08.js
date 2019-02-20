3a997f021839dd9129429546691bf8a37094afc9
study0918 <beyondthestars@yeah.net>
2019-02-20 08:04:45 +0800

    'swapClass'

--- jQuery/jQuery-files/08/08.js
+++ jQuery/jQuery-files/08/08.js
@@ -63,12 +63,35 @@
             return '';
         }
     };
+    // $.fn.swapClass = function (class1, class2) {
+    //     if (this.hasClass(class1)) {
+    //         this.removeClass(class1).addClass(class2);
+    //     } else if (this.hasClass(class2)) {
+    //         this.removeClass(class2).addClass(class1);
+    //     }
+    // };
+
+
+    // $.fn.swapClass = function (class1, class2) {
+    //     this.each(function () {
+    //         var $element = $(this);
+    //         if ($element.hasClass(class1)) {
+    //             $element.removeClass(class1).addClass(class2);
+    //         } else if ($element.hasClass(class2)) {
+    //             $element.removeClass(class2).addClass(class1);
+    //         }
+    //     })
+    // }
+
     $.fn.swapClass = function (class1, class2) {
-        if (this.hasClass(class1)) {
-            this.removeClass(class1).addClass(class2);
-        } else if (this.hasClass(class2)) {
-            this.removeClass(class2).addClass(class1);
-        }
+        return this.each(function () {
+            var $element = $(this);
+            if ($element.hasClass(class1)) {
+                $element.removeClass(class1).addClass(class2);
+            } else if ($element.hasClass(class2)) {
+                $element.removeClass(class2).addClass(class1);
+            }
+        });
     };
 })(jQuery);
 
