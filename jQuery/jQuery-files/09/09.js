489af19121441c0a6b5559fb982c4306e52c6baf
study0918 <beyondthestars@yeah.net>
2019-02-23 23:03:19 +0800

    'table'

--- jQuery/jQuery-files/09/09.js
+++ jQuery/jQuery-files/09/09.js
@@ -6,18 +6,38 @@
 //   })
 // })
 
+// $(document).ready(function () {
+//   $('#topics a').click(function (event) {
+//     event.preventDefault();
+//     var topic = $(this).text();
+//     $('#topics a.selected').removeClass('selected');
+//     $(this).addClass('selected');
+//     //console.log($(this));
+//     //$(this) 当前点中的元素
+//     $('#news tr').show();
+//     if (topic != 'All') {
+//       $('#news tr:has(td):not(:contains("' + topic + '"))')
+//         .hide();
+//     }
+//   });
+// });
+
+
+// $(document).ready(function () {
+//   $('#topics a').click(function (event) {
+//     event.preventDefault();
+//     var topic = $(this).text();
+//     $('#topics a.selected').removeClass('selected');
+//     $(this).addClass('selected');
+//     $('#news').find('tr').show();
+//     if (topic != 'All') {
+//       $('#news').find('tr:has(td)').not(function () {
+//         return $(this).children(':nth-child(4)').text() == topic;
+//       }).hide();
+//     }
+//   });
+// });
+
 $(document).ready(function () {
-  $('#topics a').click(function (event) {
-    event.preventDefault();
-    var topic = $(this).text();
-    $('#topics a.selected').removeClass('selected');
-    $(this).addClass('selected');
-    //console.log($(this));
-    //$(this) 当前点中的元素
-    $('#news tr').show();
-    if (topic != 'All') {
-      $('#news tr:has(td):not(:contains("' + topic + '"))')
-        .hide();
-    }
-  });
-});
+  $('#news').find('tr:nth-child(even)').addClass('alt')
+})
