3025d389e7d20a02d5586b2a4e190d21f5f4cbfd
study0918 <beyondthestars@yeah.net>
2019-02-24 18:33:11 +0800

    'plugin'

--- jQuery/jQuery-files/09/09.js
+++ jQuery/jQuery-files/09/09.js
@@ -48,10 +48,36 @@
 //   }).addClass('alt');
 // })
 
+// $(document).ready(function () {
+//   $('#news tbody').each(function () {
+//     $(this).children().has('td').filter(function (index) {
+//       return (index % 4) < 2;
+//     }).addClass('alt');
+//   });
+// });
+
 $(document).ready(function () {
-  $('#news tbody').each(function () {
-    $(this).children().has('td').filter(function (index) {
-      return (index % 4) < 2;
-    }).addClass('alt');
+  function stripe() {
+    $('#news').find('tr.alt').removeClass('alt');
+    $('#news tbody').each(function () {
+      $(this).children(':visible').has('td')
+        .filter(function (index) {
+          return (index % 4) < 2;
+        }).addClass('alt');
+    });
+  }
+  stripe();
+  $('#topics a').click(function (event) {
+    event.preventDefault();
+    var topic = $(this).text();
+    $('#topics a.selected').removeClass('selected');
+    $(this).addClass('selected');
+    $('#news').find('tr').show();
+    if (topic != 'All') {
+      $('#news').find('tr:has(td)').not(function () {
+        return $(this).children(':nth-child(4)').text() == topic;
+      }).hide();
+    }
+    stripe();
   });
-});
+})
