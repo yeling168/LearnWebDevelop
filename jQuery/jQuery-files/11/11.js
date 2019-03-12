fed252de37b54cf79878e8f870c718ed5944c7d6
study0918 <beyondthestars@yeah.net>
2019-03-12 21:30:18 +0800

    'animate'

--- jQuery/jQuery-files/11/11.js
+++ jQuery/jQuery-files/11/11.js
@@ -1,12 +1,65 @@
+// $(document).ready(function () {
+//     $('div.member').on('mouseenter mouseleave', function (event) {
+//         var size = event.type == 'mouseenter' ? 85 : 75;
+//         var padding = event.type == 'mouseenter' ? 0 : 5;
+//         $(this).find('img').animate({
+//             width: size,
+//             height: size,
+//             paddingTop: padding,
+//             paddingLeft: padding
+//         })
+//     })
+// })
+
+
+
+// $(document).ready(function () {
+//     $('div.member').on('mouseenter mouseleave', function (event) {
+//         var $image = $(this).find('img');
+//         if (!$image.is(':animated') || event.type == 'mouseleave') {
+//             var size = event.type == 'mouseenter' ? 85 : 75;
+//             var padding = event.type == 'mouseenter' ? 0 : 5;
+//             $(this).find('img').animate({
+//                 width: size,
+//                 height: size,
+//                 paddingTop: padding,
+//                 paddingLeft: padding
+//             })
+//         }
+
+//     })
+// })
+
+
+
+// $(document).ready(function () {
+//     $('#fx-toggle').show().on('click',function(){
+//         $.fx.off=!$.fx.off;
+//     })
+//     $('div.member').on('mouseenter mouseleave', function (event) {
+//         var size = event.type == 'mouseenter' ? 85 : 75;
+//         var padding = event.type == 'mouseenter' ? 0 : 5;
+//         $(this).find('img').stop().animate({
+//             width: size,
+//             height: size,
+//             paddingTop: padding,
+//             paddingLeft: padding
+//         });
+//     });
+// });
+
 $(document).ready(function () {
-    $('div.member').on('mouseenter mouseleave', function (event) {
-        var size = event.type == 'mouseenter' ? 85 : 75;
-        var padding = event.type == 'mouseenter' ? 0 : 5;
-        $(this).find('img').animate({
-            width: size,
-            height: size,
-            paddingTop: padding,
-            paddingLeft: padding
+    function showDetails() {
+        $(this).find('div').css({
+            display: 'block',
+            left: '-300px',
+            top: 0
+        }).each(function (index) {
+            $(this).animate({
+                left: 0,
+                top: 25 * index
+            })
         })
-    })
+    }
+    $('div.member').click(showDetails);
 })
