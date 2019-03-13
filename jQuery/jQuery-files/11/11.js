bb445bb780ca8462f43bf4a46b18ebd3837fa8f5
study0918 <beyondthestars@yeah.net>
2019-03-13 21:24:45 +0800

    'animate'

--- jQuery/jQuery-files/11/11.js
+++ jQuery/jQuery-files/11/11.js
@@ -49,6 +49,41 @@
 // });
 
 $(document).ready(function () {
+    var $movable = $('<div id="movable"></div>').appendTo('body');
+    var bioBaseStyles = {
+            display: 'none',
+            height: '5px',
+            width: '25px'
+        },
+        bioEffects = {
+            duration: 800,
+            easing: 'easeOutQuart',
+            specialEasing: {
+                opacity: 'linear'
+            }
+        };
+
+    function showBio() {
+        var $member = $(this).parent(),
+            $bio = $member.find('p.bio'),
+            startStyles = $.extend(bioBaseStyles, $member.offset()),
+            endStyles = {
+                width: $bio.width(),
+                top: $member.offset().top + 5,
+                left: $member.width() + $member.offset().left - 5,
+                opacity: 'show'
+            };
+        $movable
+            .html($bio.clone())
+            .css(startStyles)
+            .animate(endStyles, bioEffects)
+            .animate({
+                height: $bio.height(),
+            }, {
+                easing: 'easeOutQuart'
+            });
+    }
+
     function showDetails() {
         var $member = $(this);
         if ($member.hasClass('active')) {
@@ -66,13 +101,13 @@ $(document).ready(function () {
             $(this).animate({
                 left: 0,
                 top: 25 * index
-            },{
-                duration:'slow',
-                specialEasing:{
-                    top:'easeInQuart'
+            }, {
+                duration: 'slow',
+                specialEasing: {
+                    top: 'easeInQuart'
                 }
             })
-        })
+        }).promise().done(showBio);
     }
     $('div.member').click(showDetails);
 })
