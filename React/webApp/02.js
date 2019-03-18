e06330646df8ab0ea96996e4f421ed315feff28c
study0918 <beyondthestars@yeah.net>
2019-03-18 21:30:30 +0800

    'react'

---
+++ React/webApp/02.js
@@ -0,0 +1,16 @@
+//https://www.jianshu.com/p/3add54f9df17
+const List=()=>(
+    <div>
+    <Title>This is title</Title>
+    <ul>
+    <li>list item</li>
+    <li>list item</li>
+    <li>list item</li>
+    </ul>
+    </div>
+);
+
+ReactDOM.render(
+    List(),
+    document.getElementById('example')
+);
