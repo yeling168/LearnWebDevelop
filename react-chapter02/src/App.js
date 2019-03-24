4d208e0c55c5f3b0f771bccafd17e731ac3044b3
study0918 <beyondthestars@yeah.net>
2019-03-24 13:20:30 +0800

    'react'

---
+++ react-chapter02/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import PostList from './component/PostList';
+
+class App extends Component {
+  render() {
+    return ( 
+      <PostList / >
+    );
+  }
+}
+
+export default App;
