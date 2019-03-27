d8b96c338ce48354a99c39e8b38721e2e9d0115e
study0918 <beyondthestars@yeah.net>
2019-03-27 22:55:05 +0800

    'react'

--- react-chapter02/src/component/MyComponent.js
+++ react-chapter02/src/component/MyComponent.js
@@ -17,9 +17,76 @@ import React from "react";
 // }
 
 // export default MyComponent;
+//使用箭头函数
+// class MyComponent extends React.Component {
+//   //constructor通常用户初始化组件的state以及绑定事件处理方法等工作
+//   constructor(props) {
+//     super(props);
+//     this.state = { number: 0 };
+//   }
 
+//   //每点击一次Button，state中的number增加1
+
+//   handleClick(event) {
+//     const number = ++this.state.number;
+//     this.setState({
+//       number: number
+//     });
+//     console.log(this.state.number);
+//   }
+//   render() {
+//     return (
+//       <button
+//         onClick={event => {
+//           this.handleClick(event);
+//         }}
+//       >
+//         Click
+//       </button>
+//     );
+//   }
+// }
+
+// export default MyComponent;
+
+//使用组件方法
+// class MyComponent extends React.Component {
+//     //constructor通常用户初始化组件的state以及绑定事件处理方法等工作
+//     //直接将组件的方法赋值给元素的事件属性，同时在类的构造函数中，将这个方法的this 绑定到当前对象
+//     constructor(props) {
+//       super(props);
+//       this.state = { number: 0 };
+//       this.handleClick=this.handleClick.bind(this);
+//     }
+
+//     //每点击一次Button，state中的number增加1
+
+//     handleClick(event) {
+//       const number = ++this.state.number;
+//       this.setState({
+//         number: number
+//       });
+//       console.log(this.state.number);
+//     }
+//     render() {
+//       return (
+//         <button
+//           onClick={
+//             this.handleClick
+//           }
+//         >
+//           Click
+//         </button>
+//       );
+//     }
+//   }
+
+//   export default MyComponent;
+
+//有些开发者还习惯在为元素的事件属性赋值时，同时为事件处理函数绑定this
 class MyComponent extends React.Component {
   //constructor通常用户初始化组件的state以及绑定事件处理方法等工作
+  //直接将组件的方法赋值给元素的事件属性，同时在类的构造函数中，将这个方法的this 绑定到当前对象
   constructor(props) {
     super(props);
     this.state = { number: 0 };
@@ -32,16 +99,16 @@ class MyComponent extends React.Component {
     this.setState({
       number: number
     });
+    console.log(this.state.number);
   }
   render() {
     return (
-      <button
-        onClick={event => {
-          this.handleClick(event);
-        }}
-      >
-        Click
-      </button>
+      <div>
+        <div>{this.state.number}</div>
+        <button onClick={(this.handleClick = this.handleClick.bind(this))}>
+          Click
+        </button>
+      </div>
     );
   }
 }
