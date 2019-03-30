1ee28d4b47bafa1d0386b8c7284fbfe9e3dd8f3c
study0918 <beyondthestars@yeah.net>
2019-03-30 23:09:19 +0800

    'react'

---
+++ React/react-chapter03/src/component/ErrorBoundary.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false
+    };
+  }
+
+  componentDidCatch(error, info) {
+    //显示错误UI
+    this.setState({
+      hasError: true
+    });
+    //同时输出错误日志
+    console.log(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1> Oops, something went wrong. </h1>;
+    }
+    return this.props.children;
+  }
+}
