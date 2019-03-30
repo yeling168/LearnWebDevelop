820214adf57cd6ddaabfca1e39329f7b4fdee69c
study0918 <beyondthestars@yeah.net>
2019-03-30 19:05:01 +0800

    'react'

---
+++ react-chapter02/src/Bbs.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import PostList from './component/BbsPostList';
+
+class Bbs extends Component {
+  render() {
+    return ( 
+      <PostList/ >
+    );
+  }
+}
+
+export default Bbs;
--- react-chapter02/src/index.js
+++ react-chapter02/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
 import '../src/theme/index.css';
-import App from './App';
+//import App from './App';
+import Bbs from './Bbs';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Bbs />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
