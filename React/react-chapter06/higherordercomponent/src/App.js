f057641eb288c5443d6b2bb10067ea733dfe7bfa
study0918 <beyondthestars@yeah.net>
2019-04-10 21:03:20 +0800

    'react'

--- React/react-chapter06/higherordercomponent/src/App.js
+++ React/react-chapter06/higherordercomponent/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import MyComponent from "./Component/MyComponent";
+import MyComponentWithPersistentData from './Component/WithPersistentData';
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MyComponent />
+        {/* <MyComponent /> */}
+        <MyComponentWithPersistentData/>
       </div>
     );
   }
