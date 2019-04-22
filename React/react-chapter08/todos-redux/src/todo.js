762131989a2455ddd21be0211bb865844d767de4
study0918 <beyondthestars@yeah.net>
2019-04-22 21:11:38 +0800

    redux

---
+++ React/react-chapter08/todos-redux/src/todo.js
@@ -0,0 +1,10 @@
+{
+    todos:[{
+        text:'Learn React',
+        completed:true
+    },{
+        text:'Learn Redux',
+        completed:false
+    }],
+    visibilityFilter:'SHOW_COMPLETED'
+}
