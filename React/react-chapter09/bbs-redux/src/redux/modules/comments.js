809cbec6139c39efff3e7ba52c0183e621e7e755
study0918 <beyondthestars@yeah.net>
2019-05-09 21:09:48 +0800

    redux

--- React/react-chapter09/bbs-redux/src/redux/modules/comments.js
+++ React/react-chapter09/bbs-redux/src/redux/modules/comments.js
@@ -35,5 +35,71 @@ export const actions = {
         });
       }
     };
+  },
+  //新建评论
+  createComment: comment => {
+    return dispatch => {
+      dispatch(appActions.startRequest());
+      return postMessage(url.createComment(), comment).then(data => {
+        dispatch(appActions.finishRequest());
+        if (!data.error) {
+          dispatch(createCommentSuccess(data.post, data));
+        } else {
+          dispatch(appActions.setError(data.error));
+        }
+      });
+    };
   }
 };
+
+//获取评论列表成功
+const fetchCommentsSuccess = (postId, commentIds, comments, users) => ({
+  type: types.FETCH_COMMENTS,
+  postId,
+  commentIds,
+  comments,
+  user
+});
+
+//新建评论成功
+const createCommentSuccess = (postId, comment) => ({
+  type: types.CREATE_COMMENT,
+  postId,
+  comment
+});
+
+//reducers
+const byPost = (state = initialState.byPost, action) => {
+  switch (action.type) {
+    case types.FETCH_COMMENTS:
+      return { ...state, [action.postId]: action.commentIds };
+    case types.CREATE_COMMENT:
+      return {
+        ...state,
+        [action.postId]: [action.comment.id, ...state[action.postId]]
+      };
+    default:
+      return state;
+  }
+};
+
+const byId = (state = initialState.byId, action) => {
+  switch (action.type) {
+    case types.FETCH_COMMENTS:
+      return { ...state, ...action.comments };
+    case types.CREATE_COMMENT:
+      return {
+        ...state,
+        [action.comment.id]: action.comment
+      };
+    default:
+      return state;
+  }
+};
+
+const reducer = combineReducers({
+  byPost,
+  byId
+});
+
+export default reducer;
