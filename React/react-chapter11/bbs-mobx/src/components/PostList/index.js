58ada1786bd1d1287fb63bcfc6931dd18bd2dbc9
study0918 <beyondthestars@yeah.net>
2019-05-25 09:28:45 +0800

    'mobx'

--- React/react-chapter11/bbs-mobx/src/components/PostList/index.js
+++ React/react-chapter11/bbs-mobx/src/components/PostList/index.js
@@ -7,3 +7,48 @@ import PostsView from "./PostsView";
 import PostEditor from "../Post/PostEditor";
 
 import "./style.css";
+import AuthStore from "./../../stores/AuthStore";
+
+@inject("postsStore", "authStore", "uiStore")
+@observer
+class PostList extends Component {
+  componentDidMount() {
+    //获取帖子列表
+    this.props.postsStore.fetchPostList();
+  }
+  //保存新建的帖子
+  handleSave = data => {
+    this.props.postsStore
+      .createPost(data)
+      .then(() => this.props.uiStore.setAddDialogStatus(false));
+  };
+
+  //取消新建帖子的状态
+  handleCancel = () => {
+    this.props.uiStore.setAddDialogStatus(false);
+  };
+
+  //设置新建帖子的状态
+  handleNewPost = () => {
+    this.props.uiStore.setAddDialogStatus(true);
+  };
+
+  render() {
+    const { postsStore, authStore, uiStore } = this.props;
+    return (
+      <div className="postList">
+        <div>
+          <h2>帖子列表</h2>
+          {authStore.userId ? (
+            <button onClick={this.handleNewPost}>发帖</button>
+          ) : null}
+        </div>
+        {uiStore.addDialogOpen ? (
+          <PostEditor onSave={this.handleSave} onCancel={this.handleCancel} />
+        ) : null}
+      </div>
+    );
+  }
+}
+
+export default PostList;
