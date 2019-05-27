ac6fa096e24919fd4223e2043bd1b3806125940a
study0918 <beyondthestars@yeah.net>
2019-05-28 00:07:16 +0800

    'react'

--- React/newsApp/07-03/src/js/index.js
+++ React/newsApp/07-03/src/js/index.js
@@ -2,7 +2,7 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 import ComponentHeader from "./components/header";
 import ComponentFooter from "./components/footer";
-import BodyIndex from "./components/BodyIndex";
+import BodyIndex from "./components/bodyindex";
 
 class Index extends React.Component {
   render() {
