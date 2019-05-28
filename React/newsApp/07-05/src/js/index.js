79e81db251f5dea8f8a1b127a0d5d4d0b97c482e
study0918 <beyondthestars@yeah.net>
2019-05-28 21:56:13 +0800

    'react'

--- React/newsApp/07-05/src/js/index.js
+++ React/newsApp/07-05/src/js/index.js
@@ -5,6 +5,14 @@ import ComponentFooter from "./components/footer";
 import BodyIndex from "./components/bodyindex";
 
 class Index extends React.Component {
+  componentWillMount() {
+    //定义你的逻辑即可
+    console.log("Index - componentWillMount");
+  }
+
+  componentDidMount() {
+    console.log("Index - componentDidMount");
+  }
   render() {
     var component = <ComponentHeader />;
     return (
