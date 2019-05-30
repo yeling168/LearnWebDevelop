01c38b6bbfd7e973acfa62cea0ce1864e5d202f8
study0918 <beyondthestars@yeah.net>
2019-05-30 21:18:20 +0800

    react

--- React/newsApp/08-03/src/js/components/bodyindex.js
+++ React/newsApp/08-03/src/js/components/bodyindex.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import BodyChild from "./bodychild";
 
 class BodyIndex extends React.Component {
   //所有的类使用constructor
@@ -11,14 +12,32 @@ class BodyIndex extends React.Component {
       age: 20
     }; //初始化赋值
   }
+
+  changeUserInfo(age) {
+    this.setState({ age: age });
+  }
+  handleChildValueChange(event) {
+    this.setState({
+      age: event.target.value
+    });
+  }
   render() {
     return (
       <div>
         <h2>页面的主体内容</h2>
         <p>
           {this.props.userid}
-          {this.props.username} 
+          {this.props.username}
         </p>
+        <p>age:{this.state.age}</p>
+        <input
+          type="button"
+          value="提交"
+          onClick={this.changeUserInfo.bind(this, 99)}
+        />
+        <BodyChild
+          handleChildValueChange={this.handleChildValueChange.bind(this)}
+        />
       </div>
     );
   }
