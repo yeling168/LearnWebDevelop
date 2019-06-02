cd95f6cf1fce8406f972bef179722c0759fc8033
study0918 <beyondthestars@yeah.net>
2019-06-02 13:42:41 +0800

    'react'

--- React/newsApp/10-01/src/js/components/header.js
+++ React/newsApp/10-01/src/js/components/header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router";
 
 class ComponentHeader extends React.Component {
   constructor() {
@@ -26,6 +27,17 @@ class ComponentHeader extends React.Component {
     return (
       <header style={styleComponentHeader.header} className="smallFontSize">
         <h1>这里是头部</h1>
+        <ul>
+          <li>
+            <Link to={`/`}>首页</Link>
+          </li>
+          <li>
+            <Link to={`/details`}>嵌套的详情页面</Link>
+          </li>
+          <li>
+            <Link to={`/list`}>列表页面</Link>
+          </li>
+        </ul>
       </header>
     );
   }
