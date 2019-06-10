2e56f11e1adaeb8414569979263d9ee19cc6b520
study0918 <beyondthestars@yeah.net>
2019-06-10 20:44:10 +0800

    'react'

---
+++ React/newsApp/14-03/src/js/components/mobile_index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import MobileHeader from "./mobile_header";
+import MobileFooter from "./mobile_footer";
+import { Tabs } from "antd";
+const TabPane = Tabs.TabPane;
+
+export default class MobileIndex extends React.Component {
+  render() {
+    return (
+      <div>
+        <MobileHeader />
+        <Tabs>
+          <TabPane tab="头条" key="1" />
+          <TabPane tab="社会" key="2" />
+          <TabPane tab="国内" key="3" />
+          <TabPane tab="国际" key="4" />
+          <TabPane tab="娱乐" key="5" />
+        </Tabs>
+        <MobileFooter />
+      </div>
+    );
+  }
+}
