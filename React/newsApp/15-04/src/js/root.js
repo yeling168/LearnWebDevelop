d44c832c5e351c2879e42691467d637ed635f14e
study0918 <beyondthestars@yeah.net>
2019-06-12 23:41:00 +0800

    react

--- React/newsApp/15-04/src/js/root.js
+++ React/newsApp/15-04/src/js/root.js
@@ -1,25 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Button } from 'antd';
+import { Button } from "antd";
 import { Router, Route, hashHistory } from "react-router";
-import PCIndex from './components/pc_index';
-import PCNewsDetails from './components/pc_news_details';
-import MobileIndex from './components/mobile_index';
-import 'antd/dist/antd.css';
+import PCIndex from "./components/pc_index";
+import PCNewsDetails from "./components/pc_news_details";
+import MobileNewsDetails from "./components/mobile_news_details";
+import MobileIndex from "./components/mobile_index";
+import "antd/dist/antd.css";
 import MediaQuery from "react-responsive";
 
 export default class Root extends React.Component {
   render() {
     return (
       <div>
-        <MediaQuery  query='(min-device-width:1224px)'>
+        <MediaQuery query="(min-device-width:1224px)">
           <Router history={hashHistory}>
-              <Route path="/" component={PCIndex}></Route>
-              <Route path="/details/:uniquekey" component={PCNewsDetails}></Route>
+            <Route path="/" component={PCIndex} />
+            <Route path="/details/:uniquekey" component={PCNewsDetails} />
           </Router>
         </MediaQuery>
-        <MediaQuery  query='(max-device-width:1224px)'>
-          <MobileIndex/>
+        <MediaQuery query="(max-device-width:1224px)">
+          <Router history={hashHistory}>
+            <Route path="/" component={MobileIndex} />
+            <Route path="/details/:uniquekey" component={MobileNewsDetails} />
+          </Router>
         </MediaQuery>
       </div>
     );
