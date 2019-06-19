e1241a5aa4a9b9fe7b20ef86ef0beb1bc76e9d1f
study0918 <beyondthestars@yeah.net>
2019-06-19 08:01:58 +0800

    'es6'

--- ES6/tasks/css.js
+++ ES6/tasks/css.js
@@ -1,10 +1,8 @@
-import gulp from 'gulp';
-import gulpif from 'gulp-if';
-import livereload from 'gulp-livereload';
-import args from './util/args';
+import gulp from "gulp";
+import gulpif from "gulp-if";
+import livereload from "gulp-livereload";
+import args from "./util/args";
 
-gulp.task('css',()=>{
-  return gulp.src('app/**/*.css')
-    .pipe(gulp.dest('server/public'))
-
-})
+gulp.task("css", () => {
+  return gulp.src("app/**/*.css").pipe(gulp.dest("server/public"));
+});
