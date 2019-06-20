80773902154194def2bf8b260ccba5a4f3f070c2
study0918 <beyondthestars@yeah.net>
2019-06-20 23:36:45 +0800

    es6

--- ES6/server/public/js/index.js
+++ ES6/server/public/js/index.js
@@ -61,5 +61,31 @@
 
 	"use strict";
 
+	{
+	  //es5
+	  var regex = new RegExp("xyz", "i");
+	  var regex2 = new RegExp(/xyz/i);
+
+	  console.log(regex.test("xyz123"), regex2.test("xyz123"));
+
+	  //es6
+	  //第二个参数的修饰符会覆盖前面正则表达式的修饰符
+	  var regex3 = new RegExp(/xyz/gi, "i");
+	  console.log(regex3.flags);
+	}
+
+	//y,u修饰符
+	//都是全局匹配
+	//g从上一次匹配的位置继续寻找
+	//y匹配的第一个必须是紧跟着的下一个匹配成功
+	{
+	  var s = 'bbb_bb_b';
+	  var a1 = /b+/g;
+	  var a2 = new RegExp("b+", "y");
+
+	  console.log('one', a1.exec(s), a2.exec(s));
+	  console.log('two', a1.exec(s), a2.exec(s));
+	}
+
 /***/ })
 /******/ ]);
