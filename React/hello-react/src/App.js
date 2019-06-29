4c01802565d1965e935d4a4f1780135bd77664a8
study0918 <beyondthestars@yeah.net>
2019-06-29 14:11:32 +0800

    react

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -4,11 +4,15 @@ import { Home } from './components/Home';
 
 export class App extends Component{
   render(){
+      const user={
+          name:'Anna',
+          hobbies:['Sports','Reading']
+      }
       return (
           <div className="container">
             <div className="row">
                   <div className="col-xs-1 col-xs-offset-11">
-                      <Home/>
+                      <Header/>
                   </div>
               </div>
               <div className="row">
@@ -18,7 +22,9 @@ export class App extends Component{
               </div>
               <div className="row">
                   <div className="col-xs-1 col-xs-offset-11">
-                      <Header/>
+                      <Home name={'Max'} age={12} user={user}>
+                          <p>I am child</p>
+                      </Home>
                   </div>
               </div>
           </div>
--- React/hello-react/src/components/Home.js
+++ React/hello-react/src/components/Home.js
@@ -1,15 +1,35 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-export class Home extends Component{
-    render(){
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-xs-1 col-xs-offset-11">
-                        <h1>Header</h1>
-                    </div>
-                </div>
+export class Home extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <div>
+              your name is {this.props.name},your age is {this.props.age}
             </div>
-        )
-    }
+            <div>
+              <h4>hobbies</h4>
+              <ul>
+                {this.props.user.hobbies.map((hobby, i) => (
+                  <li key={i}>{hobby}</li>
+                ))}
+              </ul>
+            </div>
+            <div>{this.props.children}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+Home.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  user:PropTypes.object,
+  children:PropTypes.element.isRequired
+};
