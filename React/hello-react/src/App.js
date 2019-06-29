6a549d85b3efe2bb46691f21ba15cf93a429e136
study0918 <beyondthestars@yeah.net>
2019-06-29 17:17:15 +0800

    react

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -1,34 +1,72 @@
-import React,{Component} from 'react';
-import { Header } from './components/Header';
-import { Home } from './components/Home';
+import React, { Component } from "react";
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
 
-export class App extends Component{
-  render(){
-      const user={
-          name:'Anna',
-          hobbies:['Sports','Reading']
-      }
-      return (
-          <div className="container">
-            <div className="row">
-                  <div className="col-xs-1 col-xs-offset-11">
-                      <Header/>
-                  </div>
-              </div>
-              <div className="row">
-                  <div className="col-xs-1 col-xs-offset-11">
-                      <h1>Hello!!</h1>
-                  </div>
-              </div>
-              <div className="row">
-                  <div className="col-xs-1 col-xs-offset-11">
-                      <Home name={'Max'} age={12} user={user}>
-                          <p>I am child</p>
-                      </Home>
-                  </div>
-              </div>
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      homeLink: "Home",
+      homeMounted:true
+    };
+  }
+  onGreet(age) {
+    alert(age);
+  }
+
+  onchangeLinkName(newName) {
+    this.setState({
+      homeLink: newName
+    });
+  }
+
+  onChangeHomeMounted(){
+      this.setState({
+          homeMounted:!this.state.homeMounted
+      })
+  }
+  render() {
+    const user = {
+      name: "Anna",
+      hobbies: ["Sports", "Reading"]
+    };
+    let homeCmp="";
+        if(this.state.homeMounted){
+            homeCmp=(<Home
+                name={"Max"}
+                initialAge={12}
+                user={user}
+                greet={this.onGreet}
+                changeLink={this.onchangeLinkName.bind(this)}
+                initialName={this.state.homeLink}
+              />);
+        }
+    return (
+        
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <Header HomeLink={this.state.homeLink} />
           </div>
-      )
+        </div>
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <h1>Hello!!</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            {homeCmp}
+          </div>
+        </div>
+        <hr/>
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <button onClick={this.onChangeHomeMounted.bind(this)} className="btn btn-primary">(Un)mount Home</button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- React/hello-react/src/components/Home.js
+++ React/hello-react/src/components/Home.js
@@ -2,24 +2,121 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export class Home extends Component {
+  //初始化函数
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: props.initialAge,
+      status: 0,
+      homeLink: props.initialName
+    };
+    setTimeout(() => {
+      this.setState({
+        status: 1
+      });
+    }, 3000);
+    console.log('Constructor');
+  }
+
+  onMakeOlder() {
+    //this.setState的第一个参数是一个对象
+    this.setState({
+      age: this.state.age + 3
+    });
+    console.log(this);
+  }
+
+  handleGreet() {
+    this.props.greet(this.state.age);
+  }
+
+  onChangeLink() {
+    this.props.changeLink(this.state.homeLink);
+  }
+
+  onHandleChange(event) {
+    this.setState({
+      homeLink: event.target.value
+    });
+  }
+
+  //第一类方法
+  componentWillMount() {
+    console.log("Component will mount");
+  }
+
+  componentDidMount() {
+    console.log("Component did mount");
+  }
+
+  //第二类方法
+  componentWillReceiveProps(nextProps) {
+    console.log("Component will receive props", nextProps);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("Component should update", nextProps, nextState);
+    if(nextState.status===1){
+        return false
+    }
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log("Component will update", nextProps, nextState);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("Component did update", prevProps, prevState);
+  }
+
+  //第三类方法
+  componentWillUnmount() {
+    console.log("Component will unmount");
+  }
+
   render() {
-    console.log(this.props);
+    //console.log(this.props);
+    console.log('render');
     return (
       <div className="container">
         <div className="row">
           <div className="col-xs-1 col-xs-offset-11">
             <div>
-              your name is {this.props.name},your age is {this.props.age}
+              your name is {this.props.name},your age is {this.state.age}
             </div>
-            <div>
-              <h4>hobbies</h4>
-              <ul>
-                {this.props.user.hobbies.map((hobby, i) => (
-                  <li key={i}>{hobby}</li>
-                ))}
-              </ul>
-            </div>
-            <div>{this.props.children}</div>
+            <p>Status:{this.state.status}</p>
+            {/* ES5加括号,this.onMakeOlder()代表执行函数 */}
+            <button
+              onClick={() => {
+                this.onMakeOlder();
+              }}
+              className="btn btn-primary"
+            >
+              Make me older
+            </button>
+
+            <hr />
+            <button
+              onClick={this.handleGreet.bind(this)}
+              className="btn btn-primary"
+            >
+              Greet
+            </button>
+
+            <hr />
+            <input
+              type=""
+              value={this.state.initialName}
+              onChange={event => this.onHandleChange(event)}
+              defaultValue={this.props.initialName}
+            />
+            <button
+              onClick={this.onChangeLink.bind(this)}
+              className="btn btn-primary"
+            >
+              change Header Link
+            </button>
           </div>
         </div>
       </div>
@@ -30,6 +127,7 @@ export class Home extends Component {
 Home.propTypes = {
   name: PropTypes.string,
   age: PropTypes.number,
-  user:PropTypes.object,
-  children:PropTypes.element.isRequired
+  user: PropTypes.object,
+  greet: PropTypes.func,
+  initialName: PropTypes.string
 };
