c148291bfd78d775d2cafff0aa795c2c011d5f3e
study0918 <beyondthestars@yeah.net>
2019-06-29 12:16:11 +0800

    React

---
+++ React/hello-react/src/App.js
@@ -0,0 +1,29 @@
+import React,{Component} from 'react';
+import { Header } from './components/Header';
+import { Home } from './components/Home';
+
+export class App extends Component{
+  render(){
+      return (
+          <div className="container">
+            <div className="row">
+                  <div className="col-xs-1 col-xs-offset-11">
+                      <Home/>
+                  </div>
+              </div>
+              <div className="row">
+                  <div className="col-xs-1 col-xs-offset-11">
+                      <h1>Hello!!</h1>
+                  </div>
+              </div>
+              <div className="row">
+                  <div className="col-xs-1 col-xs-offset-11">
+                      <Header/>
+                  </div>
+              </div>
+          </div>
+      )
+  }
+}
+
+export default App;
