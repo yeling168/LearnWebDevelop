78dda37e6ecd056254ff484b51868650c003027a
study0918 <beyondthestars@yeah.net>
2019-07-05 23:18:37 +0800

    es6

--- ES6/app/js/lottery/base.js
+++ ES6/app/js/lottery/base.js
@@ -18,14 +18,209 @@ class Base {
         bonus: 19,
         tip:
           '从01-11中任选3个或多个号码，选号与奖号任意三个号相同，即中奖<em class="red">19</em>',
-        name: "任二"
+        name: "任三"
       })
       .set("r4", {
         bonus: 78,
         tip:
           '从01-11中任选4个或多个号码，所选号码与开奖号码任意四个号码相同，即中奖<em class="red">78</em>',
-        name: "任二"
+        name: "任四"
+      })
+      .set("r5", {
+        bonus: 540,
+        tip:
+          '从01-11中任选5个或多个号码，所选号码与开奖号码任意五个号码相同，即中奖<em class="red">540</em>',
+        name: "任五"
+      })
+      .set("r6", {
+        bonus: 90,
+        tip:
+          '从01-11中任选6个或多个号码，所选号码与开奖号码任意六个号码相同，即中奖<em class="red">90</em>',
+        name: "任六"
+      })
+      .set("r7", {
+        bonus: 26,
+        tip:
+          '从01-11中任选7个或多个号码，所选号码与开奖号码任意七个号码相同，即中奖<em class="red">26</em>',
+        name: "任七"
+      })
+      .set("r8", {
+        bonus: 9,
+        tip:
+          '从01-11中任选87个或多个号码，所选号码与开奖号码任意八个号码相同，即中奖<em class="red">9</em>',
+        name: "任八"
       });
+  }
+  /**
+   * [initNumber 初始化号码]
+   * @return {[type]} [description]
+   */
+  initNumber() {
+    for (let i = 1; i < 12; i++) {
+      //set对象,因为set元素不允许重复
+      this.initNumber.add("" + i).padStart(2, "0");
+    }
+  }
+
+  //设置遗漏数据
+  setOmit(omit) {
+    //保存当前对象引用
+    let self = this;
+    //omit是map对象
+    self.omit.clear(); //当前独享保留的遗漏值要清空
+    for (let [index, item] of omit.entries()) {
+      //清空遗漏的数据，保存最新的数据
+      self.omit.set(index, item);
+    }
+
+    //保存遗漏数据到页面中
+    $(self.omit_el).each(function(index, item) {
+      $(item).text(self.omit.get(index));
+    });
+  }
+
+  //设置开奖
+  /**
+   * [setOpenCode 设置开奖]
+   * @param {type} code [description]
+   */
+  setOpenCode(code) {
+    let self = this;
+    //清除当前奖号
+    self.open_code.clear();
+    for (let item of code.values()) {
+      self.open_code.add(item);
+    }
+    self.updateOpenCode && self.updateOpenCode.call(self, code);
+  }
+
+  //如何选中一个号码
+  /**
+   * 号码选中取消
+   * @param {[type]} e [description]
+   * @return {[type]}  [description]
+   */
+  toggleCodeActive(e) {
+    let self = this;
+    let $cur = $(e.currentTarget);
+    $cur.toggleClass("btn-boll-action");
+    self.getCount();
+  }
+
+  /**
+   * 切换玩法
+   * @param {*} e
+   */
+  changePlayNav(e) {
+    let self = this;
+    //currentTarget是发生在哪个DOM关联的元素
+    let $cur = $(e.currentTarget);
+    $cur
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+    self.cur_play = $cur.attr("desc").toLocaleLowerCase();
+    $("#zx_sm span").html(self.play_list.get(self.cur_play).tip);
+    $(".boll-list .btn-boll").removeClass("btn-boll-active");
+    self.getCount();
+  }
+  //操作区
+  assistHandle(e) {
+    e.preventDefault();
+    let self = this;
+    let $cur = $(e.currentTarget);
+    let index = $cur.index();
+    //清空
+    $(".boll-list .btn-boll").removeClass("btn-boll-active");
+    if (index === 0) {
+      $(".boll-list .btn-boll").addClass("btn-boll-active");
+    }
+    if (index === 1) {
+      $(".boll-list .btn-boll").each(function(i, t) {
+        if (t.textContent - 5 > 0) {
+          $(t).addClass("btn-boll-active");
+        }
+      });
+    }
+    if (index === 2) {
+      $(".boll-list .btn-boll").each(function(i, t) {
+        if (t.textContent - 6 > 0) {
+          $(t).addClass("btn-boll-active");
+        }
+      });
+    }
+    if (index === 3) {
+      $(".boll-list .btn-boll").each(function(i, t) {
+        if (t.textContent % 2 == 1) {
+          $(t).addClass("btn-boll-active");
+        }
+      });
+    }
+    if (index === 4) {
+      $(".boll-list .btn-boll").each(function(i, t) {
+        if (t.textContent % 2 == 0) {
+          $(t).addClass("btn-boll-active");
+        }
+      });
+    }
+    self.getCount();
+  }
+  //获取彩票名称
+  getName() {
+    return this.name;
+  }
+
+  //添加号码
+  addCode() {
+    let self = this;
+    let $active = $(".boll-list .btn-boll-active")
+      .text()
+      .match(/\d{2}/g);
+    let active = $active ? $active.length : 0;
+    let count = self.computeCount(active, self.cur_play);
+    if (count) {
+      self.addCodeItem(
+        $active.join(""),
+        self.cur_play,
+        self.play_list.get(self.cur_play).name,
+        count
+      );
+    }
+  }
+
+  /**
+   * 添加单词号码
+   * @param {*} code
+   * @param {*} type
+   * @param {*} typeName
+   * @param {*} count
+   */
+  addCodeItem(code, type, typeName, count) {
+    let self = this;
+    const tpl = `<li codes="${type}|${code}" bonus="${count *
+      2}" count="${count}">
+      <div class="code">
+        <b>${typeName}${count > 1 ? "复式" : "单式"}</b>
+        <b class="em">${code}</b>
+        [${count}注,<em class="code-list-money">${count * 2}</em>元]
+      </div>
+    </li>`;
+    $(self.cart_el).append(tpl);
+    self.getTotal();
+  }
+
+  getCount() {
+    let self = this;
+    let active = $(".boll-list .btn-boll-active").length;
+    let count = self.computeCount(active, self, cur_play);
+    let range = self.computeCount(active, self.cur_play);
+    let money = count * 2;
+    let win1 = range[0] - money;
+    let win2 = range[1] - money;
+    let tpl;
+    let c1 = win1 < 0 && win2 < 0 ? Math.abs(win1) : win1;
+    let c2 = win1 < 0 && win2 < 0 ? Math.abs(win2) : win2;
+    
   }
 }
 
