4e4b04aae3bb6c89239036c7045365be8e3de7ca
study0918 <beyondthestars@yeah.net>
2019-07-22 21:05:18 +0800

    vue

--- vue/vue-demo03/src/main.js
+++ vue/vue-demo03/src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
+//import App from './App.vue'
+import todoList from './todoList.vue';
+
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// })
+
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(todoList)
 })
---
+++ vue/vue-demo03/src/todoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+     <input type="text" v-model='todo'/>
+     <button @click="doAdd()">+增加</button>
+     <br>
+     <hr/>
+     <hr/>
+     <ul v-for="item in list">
+         <li>{{item}}</li>
+     </ul>
+  </div>
+</template>
+<script>
+   export default{
+       data(){
+           return {
+               todo:'',
+               list:[]
+           }
+       },
+       methods:{
+           doAdd(){
+               //获取文本框输入的值
+               //把文本框的值push到list
+               this.list.push(this.todo);
+               this.todo='';
+           }
+       }
+   }
+</script>
