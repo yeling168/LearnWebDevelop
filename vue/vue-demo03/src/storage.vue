a14e98032a94cac1bbf83393949d22684f55af5d
study0918 <beyondthestars@yeah.net>
2019-07-23 12:07:41 +0800

    vue

--- vue/vue-demo03/src/storage.vue
+++ vue/vue-demo03/src/storage.vue
@@ -1,62 +1,59 @@
 <template>
   <div id="app">
     <input type="text" v-model="todo" @keydown="doAdd($event)" />
-    <button @click="doAdd()">+增加</button>
     <br />
-    <hr />
-    <hr />
     <h2>进行中</h2>
-    <ul v-for="(item,key) in list" v-if="!item.checked">
-      <input type="checkbox" v-model="item.checked" />
-      {{item.title}}
-      <button @click="removeData(key)">删除</button>
+    <ul>
+      <li v-for="(item,key) in list" v-if="!item.checked">
+        <input type="checkbox" v-model="item.checked" @change="saveList()" />
+        {{item.title}}
+        <button @click="removeData(key)">删除</button>
+      </li>
     </ul>
     <br />
     <h2>已完成</h2>
-    <ul v-for="(item,key) in list" v-if="item.checked" class="finish">
-      <input type="checkbox" v-model="item.checked" />
-      {{item.title}}
-      <button @click="removeData(key)">删除</button>
+    <ul>
+      <li v-for="(item,key) in list" v-if="item.checked">
+        <input type="checkbox" v-model="item.checked" @change="saveList()" />
+        {{item.title}}
+        <button @click="removeData(key)">删除</button>
+      </li>
     </ul>
-    <button @click="getList()">获取list的值</button>
   </div>
 </template>
 <script>
+import storage from "./model/storage.js";
 export default {
   data() {
     return {
       todo: "",
-      list: [
-        {
-          title: "录取nodejs",
-          checked: true
-        },
-        {
-          title: "录制ionic",
-          checked: false
-        }
-      ]
+      list: []
     };
   },
   methods: {
     doAdd(e) {
-      /* 获取文本框输入的值
-               把文本框的值push到list
-               this.list.push(this.todo);
-               this.todo=''; */
-      if (e.keyCode == 13) {
+      if (e.keyCode === 13) {
         this.list.push({
           title: this.todo,
           checked: false
         });
-        this.todo='';
+        storage.set("list", this.list);
       }
     },
     removeData(key) {
       this.list.splice(key, 1);
+      storage.set("list", this.list);
     },
-    getList() {
-      console.log(this.list);
+    saveList() {
+      storage.set("list", this.list);
+    }
+  },
+  mounted: function() {
+    //生命周期函数，vue页面刷新就会触发的方法
+    var list = storage.get("list");
+    //注意判断
+    if (list) {
+      this.list = list;
     }
   }
 };
