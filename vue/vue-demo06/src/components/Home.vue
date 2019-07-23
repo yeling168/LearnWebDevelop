3b4061d132b9e1d316f124486405bfa492366e02
study0918 <beyondthestars@yeah.net>
2019-07-23 22:05:34 +0800

    vue

---
+++ vue/vue-demo06/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-header ref="header"></v-header>首页组件
+    <button @click="getChildData">获取子组件的数据和方法</button>
+  </div>
+</template>
+<script>
+import Header from "./Header";
+export default {
+  data() {
+    return {
+      msg: "我是home组件",
+      title: "首页111"
+    };
+  },
+  components: {
+    "v-header": Header
+  },
+  methods: {
+    run() {
+      alert("我是父组件的run方法");
+    },
+    getChildData(){
+      //alert(this.$refs.header.msg);
+      this.$refs.header.run();
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
