4fc417fa6999b6a7ccc47f1f1f229b88c0aad91c
study0918 <beyondthestars@yeah.net>
2019-07-25 07:27:04 +0800

    'vuex'

---
+++ vue/vue-demo23/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- 所有的内容要被根节点包含起来 -->
+  <div id="home">
+    我是首页组件 ---{{this.$store.state.count}}
+    <button @click="incCount()">增加数量+</button>
+  </div>
+</template>
+
+<script>
+//1.引入store,建议store的名字不要变
+import store from "../vuex/store";
+//注册store
+export default {
+  data() {
+    return {
+      msg: "我是一个home组件"
+    };
+  },
+  store,
+  methods: {
+    incCount() {
+      //改变vuex state里面的数据
+      console.log(this);
+      this.$store.commit('incCount');
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ vue/vue-demo23/src/components/News.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="news">
+    我是新闻组件   -----{{this.$store.state.count}}
+    <hr>
+    <button @click="incCount()">增加数量+</button>
+  </div>
+</template>
+<script>
+//引入store
+import store from '../vuex/store';
+export default {
+  data() {
+    return {
+      msg: "我是一个新闻组件",
+    };
+  },
+  store,
+  methods: {
+    incCount() {
+      //改变vuex state里面的数据
+      console.log(this);
+      this.$store.commit('incCount');
+    }
+  }
+};
+</script>
