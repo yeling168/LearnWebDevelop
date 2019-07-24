ecd297cb1d3025360c89ac931c09c33be518e0ca
study0918 <beyondthestars@yeah.net>
2019-07-24 23:26:33 +0800

    vuex

--- vue/vue-demo22/src/components/News.vue
+++ vue/vue-demo22/src/components/News.vue
@@ -1,75 +1,26 @@
 <template>
   <div id="news">
-    我是新闻组件
-    <ul
-      v-infinite-scroll="loadMore"
-      infinite-scroll-disabled="loading"
-      infinite-scroll-distance="10"
-      class="list"
-    >
-      <li v-for="item in list">{{ item.title }}</li>
-    </ul>
+    我是新闻组件   -----{{this.$store.state.count}}
+    <hr>
+    <button @click="incCount()">增加数量+</button>
   </div>
 </template>
-
-
 <script>
+//引入store
+import store from '../vuex/store';
 export default {
   data() {
     return {
       msg: "我是一个新闻组件",
-      list: [],
-      page: 1,
-      //若为真，则无限滚动不会被触发
-      loading: false
     };
   },
+  store,
   methods: {
-    loadMore() {
-      //默认会执行一次
-      this.requestData();
-    },
-    requestData() {
-      //请求数据的开关
-      this.loading = true;
-      var api =
-        "http://www.phonegap100.com/appapi.php?a=getPortalList&catid=20&page=" +
-        this.page;
-      this.$http.get(api).then(
-        response => {
-          console.log(response);
-          this.list = this.list.concat(response.body.result);
-          this.page = this.page + 1;
-          //判断最后一页是否有数据
-          if (response.body.result.length < 20) {
-            this.loading = true; //终止请求
-          } else {
-            this.loading = false; //继续请求
-          }
-        },
-        err => {
-          console.log(err);
-        }
-      );
+    incCount() {
+      //改变vuex state里面的数据
+      console.log(this);
+      this.$store.commit('incCount');
     }
   }
 };
 </script>
-
-<style lang="scss" scoped>
-.list {
-  li {
-    height: 3.4rem;
-
-    line-height: 3.4rem;
-
-    border-bottom: 1px solid #eee;
-
-    font-size: 1.6rem;
-
-    a {
-      color: #666;
-    }
-  }
-}
-</style>
