9a65f6a5f77b87448a8ba5551406005270e38a39
study0918 <beyondthestars@yeah.net>
2019-07-24 11:41:11 +0800

    'router'

---
+++ vue/vue-demo12/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>我是首页组件</h2>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "我是一个home组件",
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ vue/vue-demo12/src/main.js
@@ -0,0 +1,40 @@
+import Vue from "vue";
+import App from "./App.vue";
+//引入公共的css  注意:创建项目的时候必须用scss
+import './assets/css/basic.scss'
+
+//请求数据
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
+
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+//1.创建组件
+import Home from "./components/Home.vue";
+import News from "./components/News.vue";
+import Content from "./components/Content.vue";
+import Pcontent from "./components/Pcontent.vue";
+
+//2.配置路由
+
+const routes = [
+  { path: "/home", component: Home },
+  { path: "/news", component: News },
+  { path: "/content/:aid", component: Content } /**动态路由 */,
+  { path: "/pcontent", component: Pcontent },
+  { path: "*", redirect: "/home" } /**默认跳转路由 */
+];
+
+//实例化VueRouter
+const router = new VueRouter({
+  routes // (缩写) 相当于 routes: routes
+});
+
+new Vue({
+  el: "#app",
+  router,
+  render: h => h(App)
+});
+
+//根组件的模板里面放上<router-view></router-view>
