8fc76e609b26e459af71066c406f9a2e999519c1
study0918 <beyondthestars@yeah.net>
2019-07-27 17:23:25 +0800

    'router'

--- vue/router/passing-props/src/Router.js
+++ vue/router/passing-props/src/Router.js
@@ -1,66 +1,25 @@
 import Vue from "vue";
-
 import VueRouter from "vue-router";
+import Hello from './Hello.vue';
 
 Vue.use(VueRouter);
 
-const Home = { template: '<router-view></router-view>' }
-const Default = { template: '<div>default</div>' }
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
-const Baz = { template: '<div>baz</div>' }
-const WithParams = { template: '<div>{{ $route.params.id }}</div>' }
-const Foobar = { template: '<div>foobar</div>' }
-const FooBar = { template: '<div>FooBar</div>' }
+function dynamicPropsFn (route) {
+  const now = new Date()
+  return {
+    name: (now.getFullYear() + parseInt(route.params.years)) + '!'
+  }
+}
 
 const router = new VueRouter({
   mode: 'history',
   base: __dirname,
   routes: [
-    { path: '/', component: Home,
-      children: [
-        { path: '', component: Default },
-        { path: 'foo', component: Foo },
-        { path: 'bar', component: Bar },
-        { path: 'baz', name: 'baz', component: Baz },
-        { path: 'with-params/:id', component: WithParams },
-        // relative redirect to a sibling route
-        { path: 'relative-redirect', redirect: 'foo' }
-      ]
-    },
-    // absolute redirect
-    { path: '/absolute-redirect', redirect: '/bar' },
-    // dynamic redirect, note that the target route `to` is available for the redirect function
-    { path: '/dynamic-redirect/:id?',
-      redirect: to => {
-        const { hash, params, query } = to
-        if (query.to === 'foo') {
-          return { path: '/foo', query: null }
-        }
-        if (hash === '#baz') {
-          return { name: 'baz', hash: '' }
-        }
-        if (params.id) {
-          return '/with-params/:id'
-        } else {
-          return '/bar'
-        }
-      }
-    },
-    // named redirect
-    { path: '/named-redirect', redirect: { name: 'baz' }},
-
-    // redirect with params
-    { path: '/redirect-with-params/:id', redirect: '/with-params/:id' },
-
-    // redirect with caseSensitive
-    { path: '/foobar', component: Foobar, caseSensitive: true },
-
-    // redirect with pathToRegexpOptions
-    { path: '/FooBar', component: FooBar, pathToRegexpOptions: { sensitive: true }},
-
-    // catch all redirect
-    { path: '*', redirect: '/' }
+    { path: '/', component: Hello }, // No props, no nothing
+    { path: '/hello/:name', component: Hello, props: true }, // Pass route.params to props
+    { path: '/static', component: Hello, props: { name: 'world' }}, // static values
+    { path: '/dynamic/:years', component: Hello, props: dynamicPropsFn }, // custom logic for mapping between route and props
+    { path: '/attrs', component: Hello, props: { name: 'attrs' }}
   ]
 })
 
@@ -68,46 +27,15 @@ new Vue({
   router,
   template: `
     <div id="app">
-      <h1>Redirect</h1>
+      <h1>Route props</h1>
       <ul>
-        <li><router-link to="/relative-redirect">
-          /relative-redirect (redirects to /foo)
-        </router-link></li>
-        <li><router-link to="/relative-redirect?foo=bar">
-          /relative-redirect?foo=bar (redirects to /foo?foo=bar)
-        </router-link></li>
-        <li><router-link to="/absolute-redirect">
-          /absolute-redirect (redirects to /bar)
-        </router-link></li>
-        <li><router-link to="/dynamic-redirect">
-          /dynamic-redirect (redirects to /bar)
-        </router-link></li>
-        <li><router-link to="/dynamic-redirect/123">
-          /dynamic-redirect/123 (redirects to /with-params/123)
-        </router-link></li>
-        <li><router-link to="/dynamic-redirect?to=foo">
-          /dynamic-redirect?to=foo (redirects to /foo)
-        </router-link></li>
-        <li><router-link to="/dynamic-redirect#baz">
-          /dynamic-redirect#baz (redirects to /baz)
-        </router-link></li>
-        <li><router-link to="/named-redirect">
-          /named-redirect (redirects to /baz)
-        </router-link></li>
-        <li><router-link to="/redirect-with-params/123">
-          /redirect-with-params/123 (redirects to /with-params/123)
-        </router-link></li>
-        <li><router-link to="/foobar">
-          /foobar
-        </router-link></li>
-        <li><router-link to="/FooBar">
-          /FooBar
-        </router-link></li>
-        <li><router-link to="/not-found">
-          /not-found (redirects to /)
-        </router-link></li>
+        <li><router-link to="/">/</router-link></li>
+        <li><router-link to="/hello/you">/hello/you</router-link></li>
+        <li><router-link to="/static">/static</router-link></li>
+        <li><router-link to="/dynamic/1">/dynamic/1</router-link></li>
+        <li><router-link to="/attrs">/attrs</router-link></li>
       </ul>
-      <router-view class="view"></router-view>
+      <router-view class="view" foo="123"></router-view>
     </div>
   `
 }).$mount('#app')
