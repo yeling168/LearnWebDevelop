f4a64bfc86d1a94d02fc5e948ed358efc95af9ba
study0918 <beyondthestars@yeah.net>
2019-07-30 08:20:41 +0800

    vuex

---
+++ vue/vuex-two/vuexState/src/store.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+//访问状态对象
+const state = {
+  count: 4
+};
+//访问触发状态
+const mutations = {
+  jia(state) {
+    //console.log(state);
+    state.count++;
+  },
+  jian(state) {
+    state.count--;
+  }
+};
+export default new Vuex.Store({
+  state,
+  mutations,
+});
---
+++ vue/vuex-two/vuexState/src/vuex.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="app">
+        <h1>Hello Vue</h1>
+        <p>{{$store.state.count}}  --{{countit}}--{{count}}</p>
+        <p>
+            <button @click='$store.commit("jia")'>+</button>
+            <button @click='$store.commit("jian")'>-</button>
+        </p>
+    </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+export default {
+    name:'app',
+    data(){
+        //这里的count是组件自己的，不能和computed里面的变量重名
+        return {
+            count:0
+        }
+    },
+    /* //在组件加载之前计算值
+    computed:{
+        //这里的countit是组件自己的，不能和data里面的变量重名
+        countit(){
+            //this指的是main.js中id为app的实例化组件
+            //$store指的是store.js的实例
+            console.log(this);
+            return this.$store.state.count+1
+        }
+    } */
+
+
+    /* //简化computed
+    computed:mapState({
+       // countit:state=>state.count
+       countit:function(state){
+           console.log('state',state);
+           return state.count;
+       }
+    }) */
+
+    //更加简化computed
+   computed:mapState([
+       'countit'
+   ])
+
+}
+</script>
