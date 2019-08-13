e72d0f0ed52612d09bf3806171847d46c3716056
study0918 <beyondthestars@yeah.net>
2019-08-13 13:44:42 +0800

    vue

--- vue/vue-cli/lifecycle/src/App.vue
+++ vue/vue-cli/lifecycle/src/App.vue
@@ -1,60 +1,66 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    This Vue2.0生命周期
+    <p>TestDate:aaa={{aaa}}</p>
+    <p>
+      <button @click="add"></button>
+    </p>
   </div>
 </template>
 
 <script>
+import Hello from "./hello.vue";
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  components: { hello },
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      aaa: 2
+    };
+  },
+  methods: {
+    add() {
+      this.aaa++;
+    }
+  },
+  beforeCreate: function() {
+    //已经被显示出来了
+    console.log("1-beforeCreate 初始化之后");
+  },
+  created: function() {
+    console.log("2-created创建完成");
+  },
+  beforeMount: function() {
+    //创建好DOM但没显示出来
+    console.log("3-beforeMount挂载之前");
+  },
+  mounted: function() {
+    //DOM被创建好并显示
+    console.log("4-mounted被创建");
+  },
+  beforeUpdate: function() {
+    console.log("5-beforeUpdate数据更新前");
+  },
+  updated: function() {
+    console.log("6-updated被更新后");
+  },
+  //<keep-alive></keep-alive>
+  activated: function() {
+    console.log("7-actived");
+  },
+  deactivated: function() {
+    console.log("8-deactivated");
+  },
+  beforeDestroy() {
+    console.log("9-beforeDestory销毁之前");
+  },
+  destroyed: function() {
+    console.log("10-destoryed销毁之后");
+  },
+  watch: {
+    'aaa': function(newdata, olddata) {
+      console.log(olddata + "更改为" + newdata);
     }
   }
-}
+};
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
