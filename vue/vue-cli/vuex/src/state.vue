af29b826b16dbdcbec29e91e831aca3eb13ea5f7
study0918 <beyondthestars@yeah.net>
2019-08-17 16:31:29 +0800

    'vuex'

---
+++ vue/vue-cli/vuex/src/state.vue
@@ -0,0 +1,11 @@
+<template>
+   <div id="app">
+
+       <h1>Hello Vuex</h1>
+       <p>{{$store.state.count}} -- {{count}}</p>
+       <p>
+           <button @click="$store.commit('jia')">+</button>
+           <button @click="$store.commit('jian')">-</button>
+       </p>
+   </div>
+</template>
---
+++ vue/vue-cli/vuex/src/vuex.vue
@@ -0,0 +1,6 @@
+<template>
+    <div id="app">
+        <h1>Hello Vuex</h1>
+        <p>{{$store.state.count}}</p>
+    </div>
+</template>
