c240f4b03a296dcfc7b88bf97e8285c5d6a332cd
study0918 <beyondthestars@yeah.net>
2019-09-05 00:03:10 +0800

    vuex

--- vue/vue-cli/vuex-modules/src/store/index.js
+++ vue/vue-cli/vuex-modules/src/store/index.js
@@ -1,10 +1,14 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-import actions from './actions';
+import actions from "./actions";
+import mutations from "./mutations";
 
 export default new Vuex.Store({
-    actions
-})
+  actions,
+  modules:{
+    mutations
+  }
+});
--- vue/vue-cli/vuex-modules/src/store/mutations.js
+++ vue/vue-cli/vuex-modules/src/store/mutations.js
@@ -1,2 +1,19 @@
+import { INCREMENT,DECREMENT } from "./types";
+import getters from "./getters";
 
-import { INCREMENT } from './types';
+const state = { count: 20 };
+
+const mutations = {
+  [INCREMENT](state) {
+    state.count++;
+  },
+  [DECREMENT](state){
+      state.count--;
+  }
+};
+
+export default {
+  state,
+  mutations,
+  getters
+};
