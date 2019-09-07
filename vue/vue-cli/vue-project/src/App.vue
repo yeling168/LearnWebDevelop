a6d82704b5103878464e1607ecd0afe0e484c5ed
study0918 <beyondthestars@yeah.net>
2019-09-07 12:50:42 +0800

    vue-news

--- vue/vue-cli/vue-project/src/App.vue
+++ vue/vue-cli/vue-project/src/App.vue
@@ -1,20 +1,49 @@
 <template>
   <div id="app">
-    22
+    <loading v-if="showLoading"></loading>
+    <NavHeader></NavHeader>
+    <transition name="slide-down">
+      <router-view></router-view>
+    </transition>
+    <FooterView></FooterView>
   </div>
 </template>
 
 <script>
+import NavHeader from "./components/Nav.vue";
+import FooterView from "./components/Footer.vue";
+import { mapGetters } from "vuex";
 export default {
-  name: 'app',
-  data () {
-    return {
-      
+  name: "app",
+  data() {
+    return {};
+  },
+  components: {
+    NavHeader,
+    FooterView
+  },
+  computed: mapGetters(["showHeader", "showLoading"]),
+  watch: {
+    $route(to) {
+      if (to.path == "/user-info") {
+        this.$store.dispatch("hideHeader");
+      } else {
+        this.$store.dispatch("showHeader");
+      }
     }
   }
-}
+};
 </script>
 
-<style lang="scss">
-  @import './assets/css/index.css'
+<style>
+.slide-down-enter,
+.slide-down-leave {
+  opacity: 1;
+}
+.slide-down-enter-active,
+.slide-down-leave-active {
+  transition: 0.3s all ease;
+  opacity: 0.5;
+  transform: translate3d(0, 3em, 0);
+}
 </style>
---
+++ vue/vue-cli/vue-project/src/store/getters.js
@@ -0,0 +1,8 @@
+export default {
+  showHeader: state => {
+    return state.header;
+  },
+  showLoading: state => {
+    return state.loading;
+  }
+};
