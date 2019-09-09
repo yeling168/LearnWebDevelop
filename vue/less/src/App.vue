29152763c1fe36677cef10ab1c8d5f498a08f552
study0918 <beyondthestars@yeah.net>
2019-09-09 22:56:33 +0800

    less

--- vue/less/src/App.vue
+++ vue/less/src/App.vue
@@ -4,7 +4,6 @@
       <p>变量</p>
       <variable></variable>
     </div>
-
     <div id="mixins" v-show="isMixinsShow">
       <hr />
       <p>混合</p>
@@ -20,6 +19,16 @@
       <p>命名空间</p>
       <namespace></namespace>
     </div>
+    <div id="expression" v-show="isExpressionShow">
+      <hr />
+      <p>条件表达式</p>
+      <expression></expression>
+    </div>
+    <div id="loop" v-show="isLoopShow">
+      <hr />
+      <p>循环</p>
+      <loop></loop>
+    </div>
   </div>
 </template>
 
@@ -27,7 +36,9 @@
 import variable from "./components/variable.vue";
 import mixins from "./components/mixins.vue";
 import nestedRules from "./components/nested-rules.vue";
-import namespace from './components/namespace.vue'
+import namespace from './components/namespace.vue';
+import expression from './components/expression.vue';
+import loop from './components/loop.vue'
 export default {
   name: "app",
   data() {
@@ -35,14 +46,18 @@ export default {
       isVariableShow: false,
       isMixinsShow: false,
       isNestedRulesShow: false,
-      isNameSpaceShow:true
+      isNameSpaceShow:false,
+      isExpressionShow:false,
+      isLoopShow:true
     };
   },
   components: {
     variable,
     mixins,
     nestedRules,
-    namespace
+    namespace,
+    expression,
+    loop
   }
 };
 </script>
