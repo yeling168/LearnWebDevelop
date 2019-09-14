b6cd23bdfe25a25a28fb8e553b1b31f0a066f6d9
study0918 <beyondthestars@yeah.net>
2019-09-14 08:54:24 +0800

    'express'

--- vue/node/pcat/app.js
+++ vue/node/pcat/app.js
@@ -1,41 +1,83 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+/**
+ * Module dependencies.
+ */
+/*引用模块*/
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , util=require('util')
+  , path = require('path')
+  ,engine=require('./system');
+/*实例化express对象*/
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+/*配置app的参数和启用中间件*/
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  //告诉我们的页面模版目录
+  app.set('views', __dirname + '/views');
+  //告诉它我们使用那种模版引擎
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+//配置开发模式
+app.configure('development', function(){
+  app.use(express.errorHandler());
 });
+//指定路由控制
+app.get('/', routes.index);
+app.get('/pcat', routes.pcat);
+app.get('/user/:username',function(req,res){
+            res.send("user ："+req.params.username);
+        });
 
-module.exports = app;
+app.get('/users', user.list);
+app.all('/test/:username',function(req,res,next){
+	console.log("all methods is call");
+	//我们在这里验证用户名是否存在。
+	//如果存在直接send或者调用next(new Error('用户已经存在'));
+	//如果不存在我们调用next()把控制权交给下一个路由规则
+	next();
+	res.send('all的路由规则完毕。')
+});
+ app.get('/test/:username',function(req,res){
+     res.send("user："+req.params.username)
+})
+app.get('/abc',routes.pcat)
+//改造ejs引擎中的方法
+app.engine('ejs', engine);
+//将layout的模版布局模版设置为默认
+app.locals._layoutFile='layout'
+//片段视图
+app.get('/list',function(req,res){
+	res.render('list',{
+		title:'片段视图',
+		items:['marico',1991,'pcat']
+	})
+});
+//视图助手
+app.locals({
+	inspect:function(obj){
+		return util.inspect(obj,true)+"    解析成功";
+	}
+})
+app.get('/view',function(req,res){
+	res.locals({
+		headers:function(req,res){
+			return req.headers;
+		}
+	})
+	res.render('view',{title:"PCAT"});
+})
+//创建服务并监听端口
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
