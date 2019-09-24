8c3dc5e8cde4263bbc08d4116cd02ba55fcec4db
study0918 <beyondthestars@yeah.net>
2019-09-24 23:43:57 +0800

    vue

--- vue/juejin/vue-admin-webapp/src/components/SlideVerify/index.vue
+++ vue/juejin/vue-admin-webapp/src/components/SlideVerify/index.vue
@@ -1,5 +1,7 @@
 <template>
    <div class="slide-verify" id="slideVerify" onselectstart="return false">
        <canvas :width="w" :height="h" ref="canvas"></canvas>
+       <div @click="refresh" class="slide-vertify-refresh-icon"></div>
+       <canvas :width="w" :height="h" ref="block" class="slide-vertify-block"></canvas>
    </div>
 </template>
