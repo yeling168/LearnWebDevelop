2ca4be2b4ae8f05ffa41dfd7c800a63679f51816
study0918 <beyondthestars@yeah.net>
2019-09-25 21:11:47 +0800

    vue

--- vue/juejin/vue-admin-webapp/src/views/login/index.vue
+++ vue/juejin/vue-admin-webapp/src/views/login/index.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="login">
-     <div class="slideShadow" v-show="showSlide">
-         <transition>
-             <div class="slideSty animated bounce">
-                 <div class="iconBtn">
-                    <i class="el-icon-circle-close" @click="showSlide=false"></i>
-                    <i class="el-icon-refresh" @click="refresh"></i>
-                 </div>
-             </div>
-         <transition>
-     </div>
+    <div class="slideShadow" v-show="showSlide">
+      <transition>
+        <div class="slideSty animated bounce">
+          <div class="iconBtn">
+            <i class="el-icon-circle-close" @click="showSlide=false"></i>
+            <i class="el-icon-refresh" @click="refresh"></i>
+          </div>
+        </div>
+      </transition>
+    </div>
   </div>
 </template>
 
 <script>
-import SlideVerify from '@/components/SlideVerify'
+import SlideVerify from "@/components/SlideVerify";
 export default {
-  
-}
+  components: {
+    SlideVerify
+  }
+};
 </script>
 <style scoped lang="scss">
-
 </style>
 <style>
-
 </style>
