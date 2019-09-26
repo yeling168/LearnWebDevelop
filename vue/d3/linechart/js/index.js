2c8905af36a673e9163b670dd63c430094b91241
study0918 <beyondthestars@yeah.net>
2019-09-26 13:44:28 +0800

    d3

--- vue/d3/linechart/js/index.js
+++ vue/d3/linechart/js/index.js
@@ -7,6 +7,9 @@ var svg = d3
   .attr("width", 500)
   .attr("height", 250);
 
-d3.select("svg")
+var g=d3.select("svg")
   .append("g")
   .attr("transform", "translate(50,30)");
+
+
+d3.select('g').append('path').attr('d',line_generator);//d="M1,0L20,40L40,50L100,100L0,200"  d-path data
