ab8d32ff4beb34973959949ca07ae0c834d9362a
study0918 <beyondthestars@yeah.net>
2019-09-28 17:58:07 +0800

    d3

--- vue/d3/linechart/js/barchart-y.js
+++ vue/d3/linechart/js/barchart-y.js
@@ -1,13 +1,13 @@
 var data = [1, 4, 7, 2, 9, 13, 5, 8, 2, 9],
-  bar_height = 50,
+  bar_width = 50,
   bar_padding = 10,
-  svg_height = (bar_height + bar_padding) * data.length,
-  svg_width = 500;
+  svg_width = (bar_width + bar_padding) * data.length,
+  svg_height = 500;
 
 var scale = d3.scale
   .linear()
   .domain([0, d3.max(data)])
-  .range([0, svg_width]);
+  .range([svg_height, 0]);
 var svg = d3
   .select("#container")
   .append("svg")
@@ -21,16 +21,19 @@ var bar = svg
   .append("g")
 
   .attr("transform", function(d, i) {
-    return "translate(0," + i * (bar_height + bar_padding) + ")";
+    return "translate(" + i * (bar_width + bar_padding) + ",0)";
   });
 
 bar
   .append("rect")
   .attr({
-    width: function(d) {
+    y: function(d) {
       return scale(d);
     },
-    height: bar_height
+    width: bar_width,
+    height: function(d) {
+      return svg_height - scale(d);
+    }
   })
   .style("fill", "steelblue");
 
@@ -40,9 +43,10 @@ bar
     return d;
   })
   .attr({
-    x: function(d) {
+    y: function(d) {
       return scale(d);
     },
-    y: bar_height / 2,
-    "text-anchor":"end"
+    x: bar_width / 2,
+    "dy":15,
+    "text-anchor": "middle"
   });
