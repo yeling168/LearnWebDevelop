0e901ee9188158b34202053deaffd76b6ce41aad
study0918 <beyondthestars@yeah.net>
2019-10-18 13:41:07 +0800

    vue

--- vue/vue-bilibili/drag-dialog/src/main.js
+++ vue/vue-bilibili/drag-dialog/src/main.js
@@ -16,13 +16,13 @@ Vue.use(VueRouter);
 import Home from './components/Home.vue';
 import News from './components/News.vue';
 import User from './components/User.vue';
-import Table from './components/Table.vue';
+//import Table from './components/Table.vue';
 //2.配置路由  注意：名字
 const routes = [
   { path: '/home', component: Home },
   { path: '/news', component: News,name:'news' },
   { path: '/user', component: User},
-  { path: '/table', component: Table},
+  // { path: '/table', component: Table},
   { path: '*', redirect: '/home' }   /*默认跳转路由*/
 ]
 //3.实例化VueRouter  注意：名字
