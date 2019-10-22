4699d74c37e3d690a77322f02b39bb5805b32e5b
study0918 <beyondthestars@yeah.net>
2019-10-22 08:06:21 +0800

    瀑布图

---
+++ vue/waterfall/jQUery/js/waterfall.js
@@ -0,0 +1,28 @@
+$(window).on('load',function(){
+    waterfall();
+})
+
+function waterfall(){
+    var $boxs=$('#main>div');
+    var w=$boxs.eq(0).outerWidth();//包括padding $box
+    var cols=Math.floor($(window).width()/w);
+    $('#main').width(w*cols).css('margin','0 auto');
+    var hArr=[];
+    $boxs.each(function(index,value){
+        console.log(value);
+        var h=$boxs.eq(index).outerHeight();
+        if(index<cols){
+            hArr[index]=h;
+        }else{
+            var minH=Math.min.apply(null,hArr);
+            var minHIndex=$.inArray(minH,hArr);
+            $(value).css({
+                'position':'absolute',
+                'top':minH+'px',
+                'left':minHIndex*w+'px'
+            })
+            hArr[minHIndex]+=$boxs.eq(index).outerHeight();
+        }
+    })
+    console.log(hArr);
+}
