b75a47c5cd9d8207a16a1ad31c76faa656f7cb05
study0918 <beyondthestars@yeah.net>
2019-10-27 18:18:39 +0800

    shoppingCart

--- vue/shoppingCart/js/cart.js
+++ vue/shoppingCart/js/cart.js
@@ -1,11 +1,18 @@
 var vm=new Vue({
     el:'#app',
     data:{
+        totalMoney:0,
         productList:[]
     },
-    filters:{},
+    filters:{
+        formatMoney:function(value){
+            return "¥"+value.toFixed(2);
+        }
+    },
     mounted:function(){
-        this.cartView();
+        this.$nextTick(function(){
+            this.cartView();
+        })
     },
     methods:{
         cartView:function(){
@@ -13,8 +20,13 @@ var vm=new Vue({
             this.$http.get("data/cartData.json",{
                 "id":123
             }).then(function(res){
-                
+                _this.productList=res.body.result.list;
+                _this.totalMoney=res.body.result.totalMoney;
             })
         }
     }
+})
+
+Vue.filter("money",function(value,type){
+    return "¥"+value.toFixed(2)+type;
 })
