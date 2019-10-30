331dee4d2e519d8c696cd3d39d8b982187f390ea
study0918 <beyondthestars@yeah.net>
2019-10-30 19:42:47 +0800

    shoppingCart

--- vue/shoppingCart/js/cart.js
+++ vue/shoppingCart/js/cart.js
@@ -1,8 +1,11 @@
 var vm=new Vue({
     el:'#app',
     data:{
-        totalMoney:0,
-        productList:[]
+        totalMoney: 0,
+        productList: [],
+        checkAllFlag: false,
+        delFlag: false,
+        curProduct: ''
     },
     filters:{
         formatMoney:function(value){
@@ -23,6 +26,52 @@ var vm=new Vue({
                 _this.productList=res.body.result.list;
                 _this.totalMoney=res.body.result.totalMoney;
             })
+        },
+        changeMoney:function(product,way){
+            if(way>0){
+                product.productQuentity++;
+            }else{
+                product.productQuentity--;
+                if(product.productQuentity<1){
+                    product.productQuentity=1;
+                }
+            }
+        },
+        selectedProduct:function(item){
+            if(typeof item.checked=='undefined'){
+                this.$set(item,'checked',true);
+            }else{
+                item.checked=!item.checked;
+            }
+        },
+        checkAll:function(flag){
+            this.checkAllFlag=flag;
+            var _this=this;
+            this.productList.forEach(function(item,index){
+                if(typeof item.checked=='undefined'){
+                    _this.$set(item,'checked',_this.checkAllFlag);
+                }else{
+                    item.checked=_this.checkAllFlag;
+                }
+            })
+        },
+        calcTotalPrice:function(){
+            var _this=this;
+            this.totalMoney=0;
+            this.productList.forEach(function(item,index){
+                if(item.checked){
+                    _this.totalMoney+=item.productPrice*item.productQuantity;
+                }
+            })
+        },
+        delConfirm:function(item){
+            this.delFlag=true;
+            this.curProduct = item;
+        },
+        delProduct:function(){
+            var index=this.productList.indexOf(this.curProduct);
+            this.productList.splice(index,1);
+            this.delFlag=false;
         }
     }
 })
