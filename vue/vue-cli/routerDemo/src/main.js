7901943e5875dc66adbc4f0f28a0a55044f56ceb
study0918 <beyondthestars@yeah.net>
2020-02-15 14:01:24 +0800

    router

--- vue/vue-cli/routerDemo/src/main.js
+++ vue/vue-cli/routerDemo/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import Router from './BasicRouter/Router'
 
 new Vue({
   el: '#app',
+  Router,
   render: h => h(App)
 })
