e4199dc30a48c5a963d553b61d627179b2555d7a
study0918 <beyondthestars@yeah.net>
2020-03-07 23:30:27 +0800

    admin-app

---
+++ vue/vue-cli/admin-app/src/components/SideCollapse/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="sideCollapse" id="domColapse">
+        <i 
+        :class="{ 'el-icon-s-unfold': opened, 'el-icon-s-fold': !opened }"
+        @click="toggleOpen()">
+        </i>
+    </div>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['opened'])
+  },
+  methods: {
+    toggleOpen() {
+      this.$store.commit('app/SET_OPENED', !this.opened)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.sideCollapse i {
+  font-size: 18px;
+  color: #363f44;
+  cursor: pointer;
+}
+</style>
