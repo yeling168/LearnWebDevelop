93bcfe71f75e81cffbf7ce9ad4531bf33f9f8324
study0918 <beyondthestars@yeah.net>
2020-05-03 15:09:10 +0800

    waterfall

---
+++ vue/waterfall/jQuery/js/waterfall.js
@@ -0,0 +1,48 @@
+$(window).on('load',function(){
+    waterfall();
+    var dataInt={'data':[{'src':'1.jpg'},{'src':'2.jpg'},{'src':'3.jpg'},{'src':'4.jpg'}]};
+    $(window).on('scroll',function(){
+        if(checkScrollSlide()){
+            $.each(dataInt.data,function(key,value){
+                console.log(value);
+                var oBox=$('<div>').addClass('box').appendTo($('#main'));
+                var oPic=$('<div>').addClass('pic').appendTo($(oBox));
+                oImg=$('<img>').attr('src','images/'+$(value).attr('src')).appendTo($(oPic));
+            })
+            waterfall();
+        }
+    })
+})
+
+function waterfall(){
+    var $boxs=$('#main>div');
+    var w=$boxs.eq(0).outerWidth();//包括padding $box
+    var cols=Math.floor($(window).width()/w);
+    $('#main').width(w*cols).css('margin','0 auto');
+    var hArr=[];
+    $boxs.each(function(index,value){
+        //console.log(value);
+        var h=$boxs.eq(index).outerHeight();
+        if(index<cols){
+            hArr[index]=h;
+        }else{
+            var minH=Math.min.apply(null,hArr);
+            var minHIndex=$.inArray(minH,hArr);
+            $(value).css({
+                'position':'absolute',
+                'top':minH+'px',
+                'left':minHIndex*w+'px'
+            })
+            hArr[minHIndex]+=$boxs.eq(index).outerHeight();
+        }
+    })
+    // console.log(hArr);
+}
+
+function checkScrollSlide(){
+    var $lastBox=$('#main>div').last();
+    var lastBoxDis=$lastBox.offset().top+Math.floor($lastBox.outerHeight()/2);
+    var scrollTop=$(window).scrollTop();
+    var documentH=$(window).height();
+    return (lastBoxDis<scrollTop+documentH)?true:false;
+}
