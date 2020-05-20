43c7495e0d6406e851dbe498444962ef51ea71a2
study0918 <beyondthestars@yeah.net>
2020-05-20 17:35:26 +0800

    'topo'

--- vue/topo/src/components/vTopo/vTopo.vue
+++ vue/topo/src/components/vTopo/vTopo.vue
@@ -15,6 +15,24 @@
     </div>
     <div class="svgMain">
       <v-shapebar @click="dragShapeNode" v-show="editable"></v-shapebar>
+      <div :id="'topoId'+topoId" class="topoWrap" ref="topoWrap">
+        <svg class="topoSvg" :width="svgAttr.width" :height="svgAttr.height" @mousedown.stop="mousedownTopoSvg($event)" :viewBox="svgAttr.viewX+' '+svgAttr.viewY+' '+svgAttr.width+' '+svgAttr.height" :class="{'hand':svgAttr.isHand,'crosshair':svgAttr.isCrosshair}">
+          <defs>
+            <pattern id="Pattern" x="0" y="0" width="100" height="100" patternUnits="userSpaceOnUse">
+              <line :x1="ele.x1" :x2="ele.x2" :y1="ele.y1" :y2="ele.y2" :stroke="ele.color" :stroke-width="ele.strokeWidth" :opacity="ele.opacity" v-for="ele in gridData" :key="ele.id"></line>
+            </pattern>
+          </defs>
+          <defs>
+            <filter id="f1" x="0" y="0" width="200%" height="200%">
+              <feOffset result="offOut" in="SourceGraphic" dx="4" dy="4"></feOffset>
+              <feColorMatrix result="matrixOut" in="offOut" type="matrix" values="0.2 0 0 0 0 0 0.2 0 0 0 0 0 0.2 0 0 0 0 0 1 0" />
+              <feGaussianBlur result="blurOut" in="matrixOut" stdDeviation="2" />
+              <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
+            </filter>
+          </defs>
+          <rect fill="url(#Pattern)" :width="svgAttr.width" :height="svgAttr.height"></rect>
+        </svg>
+      </div>
     </div>
   </div>
 </template>
@@ -27,7 +45,19 @@ export default {
       editable: true,
       topoId: '',
       svgToolbar: [],
-      svgAttr: { width: 0, height: 0, isHand: false, viewX: 0, viewY: 0, minW: 0, minH: 0, isCrosshair: false }
+      svgAttr: { width: 0, height: 0, isHand: false, viewX: 0, viewY: 0, minW: 0, minH: 0, isCrosshair: false },
+      gridData: [
+        { x1: 0, x2: 100, y1: 20, y2: 20, color: '#c0c0c0', strokeWidth: 1, opacity: 0.3, id: 1 },
+        { x1: 0, x2: 100, y1: 40, y2: 40, color: '#c0c0c0', strokeWidth: 1, opacity: 0.3, id: 2 },
+        { x1: 0, x2: 100, y1: 60, y2: 60, color: '#c0c0c0', strokeWidth: 1, opacity: 0.3, id: 3 },
+        { x1: 0, x2: 100, y1: 80, y2: 80, color: '#c0c0c0', strokeWidth: 1, opacity: 0.3, id: 4 },
+        { x1: 20, x2: 20, y1: 0, y2: 100, color: '#c0c0c0', strokeWidth: 1, opacity: 0.3, id: 5 },
+        { x1: 40, x2: 40, y1: 0, y2: 100, color: '#c0c0c0', strokeWidth: 1, opacity: 0.3, id: 6 },
+        { x1: 60, x2: 60, y1: 0, y2: 100, color: '#c0c0c0', strokeWidth: 1, opacity: 0.3, id: 7 },
+        { x1: 80, x2: 80, y1: 0, y2: 100, color: '#c0c0c0', strokeWidth: 1, opacity: 0.3, id: 8 },
+        { x1: 100, x2: 100, y1: 0, y2: 100, color: '#c0c0c0', strokeWidth: 2, opacity: 0.6, id: 9 },
+        { x1: 0, x2: 100, y1: 100, y2: 100, color: '#c0c0c0', strokeWidth: 2, opacity: 0.6, id: 10 }
+      ]
     }
   },
   components: {
@@ -42,10 +72,16 @@ export default {
       let toolbarIcon = NODE.icon
       let topoEle = $(`#topoId${this.topoId}`)
       let svgOffsetLeft = topoEle.find('.topoSvg').offset().left
-      let svgOffsetTop = topoEle.find('topoSvg').offset().top
+      let svgOffsetTop = topoEle.find('.topoSvg').offset().top
       let svgWidth = topoEle.find('.topoSvg').width()
       let svgHeight = topoEle.find('.topoSvg').height()
-    }
+      let isContainSvgArea = false
+      document.onmousemove = event => {
+        let mouseX = event.clientX // 当前鼠标位置
+        let mouseY = event.clientY
+      }
+    },
+    mousedownTopoSvg() {}
   }
 }
 </script>
