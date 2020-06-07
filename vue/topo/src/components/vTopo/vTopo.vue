1e1bbdb34afe8fbcb25c914a21156deed6e785d0
study0918 <beyondthestars@yeah.net>
2020-06-07 18:07:17 +0800

    'topo'

--- vue/topo/src/components/vTopo/vTopo.vue
+++ vue/topo/src/components/vTopo/vTopo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="topoComponent">
-    <div class="svgHe" v-show="editable">
+    <div class="svgHead" v-show="editable">
       <ul class="svgHeadItemLst">
         <li class="svgHeadItem" v-for="(ele,key) in svgToolbar" :key="ele.className" :class="{'active':ele.isActive}" @mousedown="selectToolbar(key)" :title="ele.name">
           <div class="svgHeadItemImg" :class="ele.className"></div>
@@ -9,7 +9,7 @@
       <ul class="svgHeadItemLst">
         <li class="svgToolBarItem" @click="saveTopoJson" title="保存">
           <i class="fa fa-save svgToolBarIcon"></i>
-          <span class="svgToolBarTxt hidden-xs-only"></span>
+          <span class="svgToolBarTxt hidden-xs-only">保存</span>
         </li>
       </ul>
     </div>
@@ -31,8 +31,22 @@
             </filter>
           </defs>
           <rect fill="url(#Pattern)" :width="svgAttr.width" :height="svgAttr.height"></rect>
+          <g>
+            <g></g>
+            <g></g>
+            <g></g>
+          </g>
+          <line :class="{isMarkerShow:marker.isMarkerShow}" id="xmarker" class="marker" x1="0" :y1="marker.xmarkerY" :x2="marker.xmarkerX" :y2="marker.xmarkerY"></line>
+          <line :class="{isMarkerShow:marker.isMarkerShow}" id="xmarker" class="marker" :x1="marker.ymarkerX" y1="0" :x2="marker.xmarkerX" :y2="marker.xmarkerY"></line>
+          <rect></rect>
         </svg>
       </div>
+    </div>
+    <div v-if="shapebarMoveNode.isShow" class="moveNode nodeMoveCss" :style="{left:shapebarMoveNode.left+'px',top:shapebarMoveNode.top+'px'}">
+      <div class="shapeIcon">
+        <img class="shapeIconImg" :src="shapebarMoveNode.icon" />
+      </div>
+      <div class="shapeName">{{shapebarMoveNode.name}}</div>
     </div>
   </div>
 </template>
@@ -44,8 +58,25 @@ export default {
     return {
       editable: true,
       topoId: '',
-      svgToolbar: [],
+      svgToolbar: [
+        { name: '默认模式', className: 'toolbar-default', isActive: true },
+        { name: '框选模式', className: 'toolbar-rectangle_selection', isActive: false }
+      ],
+      shapebarMoveNode: {
+        left: 0,
+        top: 0,
+        name: '',
+        icon: '',
+        isShow: false
+      },
       svgAttr: { width: 0, height: 0, isHand: false, viewX: 0, viewY: 0, minW: 0, minH: 0, isCrosshair: false },
+      marker: {
+        xmarkerY: 0,
+        xmarkerX: 0,
+        ymarkerX: 0,
+        ymarkerY: 0,
+        isMarkerShow: false
+      },
       gridData: [
         { x1: 0, x2: 100, y1: 20, y2: 20, color: '#c0c0c0', strokeWidth: 1, opacity: 0.3, id: 1 },
         { x1: 0, x2: 100, y1: 40, y2: 40, color: '#c0c0c0', strokeWidth: 1, opacity: 0.3, id: 2 },
@@ -64,6 +95,13 @@ export default {
     vShapebar
   },
   methods: {
+    GenNonDuplicateID(randomLength) {
+      return Number(
+        Math.random()
+          .toString()
+          .substr(3, randomLength) + Date.now()
+      ).toString(36)
+    },
     saveTopoJson() {},
     dragShapeNode(nodeData, key, event) {
       let NODE = nodeData[key]
@@ -71,20 +109,183 @@ export default {
       let toolbarName = NODE.type
       let toolbarIcon = NODE.icon
       let topoEle = $(`#topoId${this.topoId}`)
-      let svgOffsetLeft = topoEle.find('.topoSvg').offset().left
-      let svgOffsetTop = topoEle.find('.topoSvg').offset().top
-      let svgWidth = topoEle.find('.topoSvg').width()
-      let svgHeight = topoEle.find('.topoSvg').height()
+      let svgOffsetLeft = topoEle.find('.topoSvg').offset().left //251
+      let svgOffsetTop = topoEle.find('.topoSvg').offset().top // 36
+      let svgWidth = topoEle.find('.topoSvg').width() //1097
+      let svgHeight = topoEle.find('.topoSvg').height() //758
       let isContainSvgArea = false
       document.onmousemove = event => {
+        console.log('scrollLeft', $(document).scrollLeft())
+        console.log('scrollTop', $(document).scrollTop())
         let mouseX = event.clientX // 当前鼠标位置
+        console.log('clientX', mouseX)
         let mouseY = event.clientY
+        console.log('clientY', mouseY)
+        let nodeX = event.clientX - svgOffsetLeft + $(document).scrollLeft() + this.svgAttr.viewX //svg最终位置
+        let nodeY = event.clientY - svgOffsetTop + $(document).scrollTop() + this.svgAttr.viewY
+        isContainSvgArea = false
+        this.shapebarMoveNode.left = mouseX + 4 + $(document).scrollLeft() //鼠标位置+文档滚动的距离
+        this.shapebarMoveNode.top = mouseY + 4 + $(document).scrollTop()
+        this.shapebarMoveNode.name = toolbarName
+        this.shapebarMoveNode.icon = toolbarIcon
+        this.shapebarMoveNode.isShow = true
+        //console.log('---', this.shapebarMoveNode)
+        this.marker.isMarkerShow = false
+        // 鼠标滑入svg区域内显示标尺并显示标尺正确位置
+        if (mouseX >= svgOffsetLeft && mouseX <= svgOffsetLeft + svgWidth && mouseY >= svgOffsetTop - $(document).scrollTop() && mouseY <= svgOffsetTop + svgHeight - $(document).scrollTop()) {
+          this.marker.isMarkerShow = true
+          isContainSvgArea = true
+          let n1 = Math.floor(nodeX / 20) // grid宽高的整数倍
+          let n2 = Math.floor(nodeY / 20)
+          this.marker.xmarkerY = n2 * 20
+          this.marker.ymarkerY = n1 * 20
+        }
+      }
+      document.onmouseup = event => {
+        document.onmousemove = null
+        document.onmouseup = null
+        // 重新初始toolbarMoveNode的值
+        this.shapebarMoveNode.left = 0
+        this.shapebarMoveNode.top = 0
+        this.shapebarMoveNode.name = ''
+        this.shapebarMoveNode.icon = ''
+        this.shapebarMoveNode.isShow = false
       }
     },
-    mousedownTopoSvg() {}
+    mousedownTopoSvg() {},
+    initTopoWH() {
+      this.$nextTick(() => {
+        let ele = `#topoId${this.topoId}`
+        let topoW = $(ele).width()
+        let topoH = $(ele).height()
+        this.marker.xmarkerX = topoW
+        this.marker.ymarkerY = topoH
+        this.svgAttr.width = topoW
+        this.svgAttr.height = topoH
+        this.svgAttr.minW = topoW
+        this.svgAttr.minH = topoH
+      })
+    }
+  },
+  mounted() {
+    this.topoId = this.GenNonDuplicateID(5)
+    this.initTopoWH() // 初始化topo组件宽高
   }
 }
 </script>
 
-<style>
+<style scoped lang="less">
+@svg-common-color: #768699;
+@stroke-width: 2;
+@stroke-select-width: 3;
+@stroke-select-color: red;
+@border-color: #aaaaaa;
+@storke-dasharray: 5, 5;
+/*svgMain*/
+.svgMain {
+  height: 100%;
+  box-sizing: border-box;
+  display: flex;
+  flex: 1;
+}
+.moveNode {
+  position: absolute;
+  border: 1px solid @svg-common-color;
+  box-sizing: border-box;
+  &.nodeMoveCss {
+    width: 57px;
+    height: 57px;
+    background-color: #fff;
+    -webkit-user-select: none;
+    user-select: none;
+    box-sizing: border-box;
+    padding: 5px;
+  }
+}
+.topoComponent {
+  width: 100%;
+  box-sizing: border-box;
+  background-color: #fff;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.svgHead {
+  width: 100%;
+  height: 40px;
+  box-sizing: border-box;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: @theme-color;
+  border: solid @border-color;
+  border-width: 1px 1px 0;
+  box-shadow: inset 0 1px 0 0 #fff;
+  .svgHeadItemLst {
+    display: flex;
+    .svgHeadItem {
+      padding: 5px 10px;
+      border: 1px solid @border-color;
+      cursor: pointer;
+      list-style: none;
+      border-left-width: 0;
+      &:hover {
+        background-color: #ebebeb;
+      }
+      &:first-child {
+        border-left-width: 1px;
+      }
+      &.active {
+        background-color: #ebebeb;
+        box-shadow: 2px 2px 1px #ccc inset;
+      }
+      .svgHeadItemImg {
+        background: url('../../assets/topo/icons.png');
+        width: 16px;
+        height: 16px;
+        background-size: 479px 16px;
+        &.toolbar-default {
+          background-position: -16px 0px;
+        }
+        &.toolbar-rectangle_selection {
+          background-position: -294px 0px;
+        }
+        &.toolbar-zoomin {
+          background-position: -425px 0px;
+        }
+        &.toolbar-zoomout {
+          background-position: -444px 0px;
+        }
+        &.toolbar-zoomreset {
+          background-position: -462px 0px;
+        }
+      }
+    }
+  }
+}
+.topoWrap {
+  flex: 1;
+  width: 100%;
+  box-sizing: border-box;
+  border: 1px solid @border-color;
+  overflow: hidden;
+  position: relative;
+  background: #fff;
+  .topoSvg {
+    box-sizing: border-box;
+    background-color: #fff;
+    -webkit-user-select: none;
+    user-select: none;
+    -moz-select: none;
+    -ms-select: none;
+    -o-select: none;
+    &.hand {
+      cursor: pointer;
+    }
+    &.crosshair {
+      cursor: crosshair;
+    }
+  }
+}
 </style>
