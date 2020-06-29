80ed973c85b2f9eca2ef49d8b51a891cb1fa860a
study0918 <beyondthestars@yeah.net>
2020-06-30 07:17:26 +0800

    webpack

--- vue/vue-cli/beginwebpack/webpack.config.js
+++ vue/vue-cli/beginwebpack/webpack.config.js
@@ -1,4 +1,5 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   devtool: "eval-source-map",
   entry: __dirname + "/app/main.js",
@@ -10,9 +11,11 @@ module.exports = {
     contentBase: "./public", //本地服务器所加载的页面所在的目录
     historyApiFallback: true, //不跳转
     inline: true, //实时刷新
+    hot: true
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /(\.jsx|\.js)$/,
         use: {
           loader: "babel-loader",
@@ -21,7 +24,8 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [{
+        use: [
+          {
             loader: "style-loader",
           },
           {
@@ -32,13 +36,17 @@ module.exports = {
             },
           },
           {
-            loader: "postcss-loader"
-          }
+            loader: "postcss-loader",
+          },
         ],
       },
     ],
   },
   plugins: [
-    new webpack.BannerPlugin('版权所有，翻版必究')
-  ]
+    new webpack.BannerPlugin("版权所有，翻版必究"),
+    new HtmlWebpackPlugin({
+      template: __dirname + "/app/index.tmpl.html", //new 一个这个插件的实例，并传入相关的参数
+    }),
+    new webpack.HotModuleReplacementPlugin()//热加载插件
+  ],
 };
