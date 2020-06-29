0a694d292facb0db491d7cfde5902050468677c3
study0918 <beyondthestars@yeah.net>
2020-06-29 21:00:10 +0800

    'webpack'

--- vue/vue-cli/beginwebpack/app/Greeter.js
+++ vue/vue-cli/beginwebpack/app/Greeter.js
@@ -1,13 +1,19 @@
 //Greeter,js
-import React, {Component} from 'react'
+import React, {
+  Component
+} from 'react'
 import config from './config.json';
+import styles from './Greeter.css'; //导入
 
-class Greeter extends Component{
+class Greeter extends Component {
   render() {
-    return (
-      <div>
-        {config.greetText}
-      </div>
+    return ( <
+      div className = {
+        styles.root
+      } > {
+        config.greetText
+      } <
+      /div>
     );
   }
 }
--- vue/vue-cli/beginwebpack/webpack.config.js
+++ vue/vue-cli/beginwebpack/webpack.config.js
@@ -19,6 +19,21 @@ module.exports = {
         },
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true, // 指定启用css modules
+              localIdentName: "[name]__[local]--[hash:base64:5]", // 指定css的类名格式
+            },
+          },
+        ],
+      },
     ],
   },
 };
