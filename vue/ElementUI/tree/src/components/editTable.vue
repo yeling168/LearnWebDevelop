82f1731a24f099584d2307ad8f3438e774b05356
study0918 <beyondthestars@yeah.net>
2020-07-02 14:29:26 +0800

    edittable

---
+++ vue/ElementUI/tree/src/components/editTable.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <el-button @click="$refs.editable.insert()">新增</el-button>
+    <el-button @click="$refs.editable.removeSelecteds()">删除选中</el-button>
+
+    <elx-editable ref="editable" :data.sync="tableData">
+      <elx-editable-column type="selection" width="55"></elx-editable-column>
+      <elx-editable-column type="index" width="55"></elx-editable-column>
+      <elx-editable-column prop="name" label="只读"></elx-editable-column>
+      <elx-editable-column
+        prop="sex"
+        label="下拉"
+        :edit-render="{ name: 'ElSelect', options: sexList }"
+      ></elx-editable-column>
+      <elx-editable-column
+        prop="num"
+        label="数值"
+        :edit-render="{ name: 'ElInputNumber' }"
+      ></elx-editable-column>
+      <elx-editable-column
+        prop="date"
+        label="日期"
+        :edit-render="{
+          name: 'ElDatePicker',
+          props: { type: 'date', format: 'yyyy-MM-dd' }
+        }"
+      ></elx-editable-column>
+      <elx-editable-column
+        prop="flag"
+        label="开关"
+        :edit-render="{ name: 'ElSwitch', type: 'visible' }"
+      ></elx-editable-column>
+      <elx-editable-column
+        prop="remark"
+        label="文本"
+        :edit-render="{ name: 'ElInput' }"
+      ></elx-editable-column>
+    </elx-editable>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          date: 1551322088449,
+          name: "小徐",
+          sex: "1",
+          num: "26",
+          flag: false,
+          remark: "备注"
+        }
+      ],
+      sexList: [
+        {
+          label: "男",
+          value: "1"
+        },
+        {
+          label: "女",
+          value: "0"
+        }
+      ]
+    };
+  }
+};
+</script>
